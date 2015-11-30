749ebc8015953011992fcd7012c087c38f0ba263
Marcos Maroñas <marcos.maronasbravo@bsc.es>
2015-11-30 11:13:48 +0100

    When nodeSize is 1, nodeRank is always 0. Therefore, if there are no heads, nodeFlag becomes always 0 and so, the temporal directory where the checkpoints are being stored is not renamed and then, in the restart, FTI is not able to find the checkpoints, although they exist, because they are not in the expected directory. Changing the condition "nodeRank==1" to "nodeRank==0" when setting nodeFlag, this issue is solved and the checkpoint/restart works as expected.

--- src/checkpoint.c
+++ src/checkpoint.c
@@ -197,7 +197,7 @@ int FTI_PostCkpt(int group, int fo, int pr) {
     t2 = MPI_Wtime();
     FTI_GroupClean(FTI_Exec.ckptLvel, group, pr);
     MPI_Barrier(FTI_COMM_WORLD);
-    nodeFlag = (((!FTI_Topo.amIaHead) && (FTI_Topo.nodeRank == 1)) || (FTI_Topo.amIaHead))? 1 : 0;
+    nodeFlag = (((!FTI_Topo.amIaHead) && (FTI_Topo.nodeRank == 0)) || (FTI_Topo.amIaHead))? 1 : 0;
     if (nodeFlag)
     {
         level = (FTI_Exec.ckptLvel != 4) ? FTI_Exec.ckptLvel : 1;
