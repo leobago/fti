2a1c9165f196d0180cd8cc32183af756545dab43
Sławek Zdanowski <slawek@man.poznan.pl>
2016-02-01 18:59:03 +0100

    applying clang-format 'webkit' style to the fti source files

--- src/checkpoint.c
+++ src/checkpoint.c
@@ -5,9 +5,7 @@
  *  @brief  Checkpointing functions for the FTI library.
  */
 
-
 #include "fti.h"
-
 
 /*-------------------------------------------------------------------------*/
 /**
@@ -20,37 +18,34 @@
 
  **/
 /*-------------------------------------------------------------------------*/
-int FTI_UpdateIterTime() {
+int FTI_UpdateIterTime()
+{
     int nbProcs, res;
     char str[FTI_BUFS];
     double last = FTI_Exec.iterTime;
     FTI_Exec.iterTime = MPI_Wtime();
-    if (FTI_Exec.ckptIcnt > 0)
-    {
+    if (FTI_Exec.ckptIcnt > 0) {
         FTI_Exec.lastIterTime = FTI_Exec.iterTime - last;
         FTI_Exec.totalIterTime = FTI_Exec.totalIterTime + FTI_Exec.lastIterTime;
-        if (FTI_Exec.ckptIcnt % FTI_Exec.syncIter == 0)
-        {
+        if (FTI_Exec.ckptIcnt % FTI_Exec.syncIter == 0) {
             FTI_Exec.meanIterTime = FTI_Exec.totalIterTime / FTI_Exec.ckptIcnt;
             MPI_Allreduce(&FTI_Exec.meanIterTime, &FTI_Exec.globMeanIter, 1, MPI_DOUBLE, MPI_SUM, FTI_COMM_WORLD);
             MPI_Comm_size(FTI_COMM_WORLD, &nbProcs);
-            FTI_Exec.globMeanIter = FTI_Exec.globMeanIter/nbProcs;
-            if (FTI_Exec.globMeanIter > 60)
-            {
+            FTI_Exec.globMeanIter = FTI_Exec.globMeanIter / nbProcs;
+            if (FTI_Exec.globMeanIter > 60) {
                 FTI_Exec.ckptIntv = 1;
-            } else {
-                FTI_Exec.ckptIntv = (1*60)/FTI_Exec.globMeanIter;
+            }
+            else {
+                FTI_Exec.ckptIntv = (1 * 60) / FTI_Exec.globMeanIter;
             }
             res = FTI_Exec.ckptLast + FTI_Exec.ckptIntv;
-            if (res >= FTI_Exec.ckptIcnt)
-            {
+            if (res >= FTI_Exec.ckptIcnt) {
                 FTI_Exec.ckptNext = res;
             }
-            if (FTI_Exec.syncIter < (FTI_Exec.ckptIntv/2))
-            {
+            if (FTI_Exec.syncIter < (FTI_Exec.ckptIntv / 2)) {
                 FTI_Exec.syncIter = FTI_Exec.syncIter * 2;
                 sprintf(str, "Iteration frequency : %.2f sec/iter => %d iter/min. Resync every %d iter.",
-                        FTI_Exec.globMeanIter, FTI_Exec.ckptIntv, FTI_Exec.syncIter);
+                    FTI_Exec.globMeanIter, FTI_Exec.ckptIntv, FTI_Exec.syncIter);
                 FTI_Print(str, FTI_DBUG);
             }
         }
@@ -58,7 +53,6 @@ int FTI_UpdateIterTime() {
     FTI_Exec.ckptIcnt++; // Increment checkpoint loop counter
     return FTI_SCES;
 }
-
 
 /*-------------------------------------------------------------------------*/
 /**
@@ -72,52 +66,47 @@ int FTI_UpdateIterTime() {
 
  **/
 /*-------------------------------------------------------------------------*/
-int FTI_WriteCkpt(FTIT_dataset* FTI_Data) {
+int FTI_WriteCkpt(FTIT_dataset* FTI_Data)
+{
     int i, res;
-    FILE *fd;
+    FILE* fd;
     double tt = MPI_Wtime();
     char fn[FTI_BUFS], str[FTI_BUFS];
     snprintf(FTI_Exec.ckptFile, FTI_BUFS, "Ckpt%d-Rank%d.fti", FTI_Exec.ckptID, FTI_Topo.myRank);
-    if (FTI_Ckpt[4].isInline && FTI_Exec.ckptLvel == 4)
-    {
-        sprintf(fn,"%s/%s",FTI_Conf.gTmpDir, FTI_Exec.ckptFile);
+    if (FTI_Ckpt[4].isInline && FTI_Exec.ckptLvel == 4) {
+        sprintf(fn, "%s/%s", FTI_Conf.gTmpDir, FTI_Exec.ckptFile);
         mkdir(FTI_Conf.gTmpDir, 0777);
-    } else {
-        sprintf(fn,"%s/%s",FTI_Conf.lTmpDir, FTI_Exec.ckptFile);
+    }
+    else {
+        sprintf(fn, "%s/%s", FTI_Conf.lTmpDir, FTI_Exec.ckptFile);
         mkdir(FTI_Conf.lTmpDir, 0777);
     }
     fd = fopen(fn, "wb");
-    if (fd == NULL)
-    {
+    if (fd == NULL) {
         FTI_Print("FTI checkpoint file could not be opened.", FTI_EROR);
         return FTI_NSCS;
     }
-    for(i = 0; i < FTI_Exec.nbVar; i++)
-    {
-        if (fwrite(FTI_Data[i].ptr, FTI_Data[i].eleSize, FTI_Data[i].count, fd) != FTI_Data[i].count)
-        {
+    for (i = 0; i < FTI_Exec.nbVar; i++) {
+        if (fwrite(FTI_Data[i].ptr, FTI_Data[i].eleSize, FTI_Data[i].count, fd) != FTI_Data[i].count) {
             sprintf(str, "Dataset #%d could not be written.", FTI_Data[i].id);
             FTI_Print(str, FTI_EROR);
             return FTI_NSCS;
         }
     }
-    if (fflush(fd) != 0)
-    {
+    if (fflush(fd) != 0) {
         FTI_Print("FTI checkpoint file could not be flushed.", FTI_EROR);
         return FTI_NSCS;
     }
-    if (fclose(fd) != 0)
-    {
+    if (fclose(fd) != 0) {
         FTI_Print("FTI checkpoint file could not be flushed.", FTI_EROR);
         return FTI_NSCS;
     }
-    sprintf(str, "Time writing checkpoint file : %f seconds.", MPI_Wtime()-tt);
+    sprintf(str, "Time writing checkpoint file : %f seconds.", MPI_Wtime() - tt);
     FTI_Print(str, FTI_DBUG);
     int globalTmp = (FTI_Ckpt[4].isInline && FTI_Exec.ckptLvel == 4) ? 1 : 0;
     res = FTI_Try(FTI_CreateMetadata(globalTmp), "create metadata.");
     return res;
 }
-
 
 /*-------------------------------------------------------------------------*/
 /**
@@ -135,21 +124,20 @@ int FTI_WriteCkpt(FTIT_dataset* FTI_Data) {
 
  **/
 /*-------------------------------------------------------------------------*/
-int FTI_GroupClean(int level, int group, int pr) {
+int FTI_GroupClean(int level, int group, int pr)
+{
     int i, rank;
-    if (level == 0)
-    {
+    if (level == 0) {
         FTI_Print("Error postprocessing checkpoint. Discarding checkpoint...", FTI_WARN);
     }
     rank = FTI_Topo.myRank;
-    for(i = 0; i < pr; i++)
-    {
-        if (FTI_Topo.amIaHead) rank = FTI_Topo.body[i];
-        FTI_Clean(level, i+group, rank);
+    for (i = 0; i < pr; i++) {
+        if (FTI_Topo.amIaHead)
+            rank = FTI_Topo.body[i];
+        FTI_Clean(level, i + group, rank);
     }
     return FTI_SCES;
 }
-
 
 /*-------------------------------------------------------------------------*/
 /**
@@ -167,59 +155,62 @@ int FTI_GroupClean(int level, int group, int pr) {
 
  **/
 /*-------------------------------------------------------------------------*/
-int FTI_PostCkpt(int group, int fo, int pr) {
+int FTI_PostCkpt(int group, int fo, int pr)
+{
     int i, tres, res, level, nodeFlag, globalFlag = FTI_Topo.splitRank;
     double t0, t1, t2, t3;
     char str[FTI_BUFS];
     t0 = MPI_Wtime();
-    res = (FTI_Exec.ckptLvel == (FTI_REJW-FTI_BASE)) ? FTI_NSCS : FTI_SCES;
+    res = (FTI_Exec.ckptLvel == (FTI_REJW - FTI_BASE)) ? FTI_NSCS : FTI_SCES;
     MPI_Allreduce(&res, &tres, 1, MPI_INT, MPI_SUM, FTI_COMM_WORLD);
-    if (tres != FTI_SCES)
-    {
+    if (tres != FTI_SCES) {
         FTI_GroupClean(0, group, pr);
         return FTI_NSCS;
     }
     t1 = MPI_Wtime();
-    for(i = 0; i < pr; i++) {
-        switch(FTI_Exec.ckptLvel) {
-            case 4 : res += FTI_Flush(i+group, fo); break;
-            case 3 : res += FTI_RSenc(i+group); break;
-            case 2 : res += FTI_Ptner(i+group); break;
-            case 1 : res += FTI_Local(i+group); break;
+    for (i = 0; i < pr; i++) {
+        switch (FTI_Exec.ckptLvel) {
+        case 4:
+            res += FTI_Flush(i + group, fo);
+            break;
+        case 3:
+            res += FTI_RSenc(i + group);
+            break;
+        case 2:
+            res += FTI_Ptner(i + group);
+            break;
+        case 1:
+            res += FTI_Local(i + group);
+            break;
         }
     }
     MPI_Allreduce(&res, &tres, 1, MPI_INT, MPI_SUM, FTI_COMM_WORLD);
-    if (tres != FTI_SCES)
-    {
+    if (tres != FTI_SCES) {
         FTI_GroupClean(0, group, pr);
         return FTI_NSCS;
     }
     t2 = MPI_Wtime();
     FTI_GroupClean(FTI_Exec.ckptLvel, group, pr);
     MPI_Barrier(FTI_COMM_WORLD);
-    nodeFlag = (((!FTI_Topo.amIaHead) && (FTI_Topo.nodeRank == 0)) || (FTI_Topo.amIaHead))? 1 : 0;
-    if (nodeFlag)
-    {
+    nodeFlag = (((!FTI_Topo.amIaHead) && (FTI_Topo.nodeRank == 0)) || (FTI_Topo.amIaHead)) ? 1 : 0;
+    if (nodeFlag) {
         level = (FTI_Exec.ckptLvel != 4) ? FTI_Exec.ckptLvel : 1;
         rename(FTI_Conf.lTmpDir, FTI_Ckpt[level].dir);
         FTI_Print("Local directory renamed", FTI_DBUG);
     }
-    if (!globalFlag)
-    {
-        if (FTI_Exec.ckptLvel == 4)
-        {
+    if (!globalFlag) {
+        if (FTI_Exec.ckptLvel == 4) {
             rename(FTI_Conf.gTmpDir, FTI_Ckpt[FTI_Exec.ckptLvel].dir);
         }
         rename(FTI_Conf.mTmpDir, FTI_Ckpt[FTI_Exec.ckptLvel].metaDir);
     }
     t3 = MPI_Wtime();
-    sprintf(str, "Post-checkpoint took %.2f sec.", t3-t0);
-    sprintf(str, "%s (Ag:%.2fs, Pt:%.2fs, Cl:%.2fs)", str, t1-t0, t2-t1, t3-t2);
+    sprintf(str, "Post-checkpoint took %.2f sec.", t3 - t0);
+    sprintf(str, "%s (Ag:%.2fs, Pt:%.2fs, Cl:%.2fs)", str, t1 - t0, t2 - t1, t3 - t2);
     FTI_Print(str, FTI_INFO);
 
     return FTI_SCES;
 }
-
 
 /*-------------------------------------------------------------------------*/
 /**
@@ -233,50 +224,41 @@ int FTI_PostCkpt(int group, int fo, int pr) {
 
  **/
 /*-------------------------------------------------------------------------*/
-int FTI_Listen() {
+int FTI_Listen()
+{
     MPI_Status status;
     char str[FTI_BUFS];
     int i, buf, res, flags[7];
-    for (i = 0; i < 7; i++)
-    { // Initialize flags
+    for (i = 0; i < 7; i++) { // Initialize flags
         flags[i] = 0;
     }
     FTI_Print("Head listening...", FTI_DBUG);
-    for(i = 0; i < FTI_Topo.nbApprocs; i++)
-    { // Iterate on the application processes in the node
+    for (i = 0; i < FTI_Topo.nbApprocs; i++) { // Iterate on the application processes in the node
         MPI_Recv(&buf, 1, MPI_INT, FTI_Topo.body[i], FTI_Conf.tag, FTI_Exec.globalComm, &status);
         sprintf(str, "The head received a %d message", buf);
         FTI_Print(str, FTI_DBUG);
         fflush(stdout);
-        flags[buf-FTI_BASE] = flags[buf-FTI_BASE] + 1;
+        flags[buf - FTI_BASE] = flags[buf - FTI_BASE] + 1;
     }
-    for (i = 1; i < 7; i++)
-    {
-        if (flags[i] == FTI_Topo.nbApprocs)
-        { // Determining checkpoint level
+    for (i = 1; i < 7; i++) {
+        if (flags[i] == FTI_Topo.nbApprocs) { // Determining checkpoint level
             FTI_Exec.ckptLvel = i;
         }
     }
-    if (flags[6] > 0)
-    {
+    if (flags[6] > 0) {
         FTI_Exec.ckptLvel = 6;
     }
-    if (FTI_Exec.ckptLvel == 5)
-    { // If we were asked to finalize
+    if (FTI_Exec.ckptLvel == 5) { // If we were asked to finalize
         return FTI_ENDW;
     }
     res = FTI_Try(FTI_PostCkpt(1, 0, FTI_Topo.nbApprocs), "postprocess the checkpoint.");
-    if (res == FTI_SCES)
-    {
+    if (res == FTI_SCES) {
         FTI_Exec.wasLastOffline = 1;
         FTI_Exec.lastCkptLvel = FTI_Exec.ckptLvel;
         res = FTI_Exec.ckptLvel;
     }
-    for(i = 0; i < FTI_Topo.nbApprocs; i++)
-    { // Send msg. to avoid checkpoint collision
+    for (i = 0; i < FTI_Topo.nbApprocs; i++) { // Send msg. to avoid checkpoint collision
         MPI_Send(&res, 1, MPI_INT, FTI_Topo.body[i], FTI_Conf.tag, FTI_Exec.globalComm);
     }
     return FTI_SCES;
 }
-
-
--- src/tools.c
+++ src/tools.c
@@ -5,11 +5,9 @@
  *  @brief  Utility functions for the FTI library.
  */
 
-
 #include "fti.h"
 #include <dirent.h>
 #include <sys/types.h>
-
 
 int FTI_Clean(int level, int group, int rank);
 
@@ -27,34 +25,31 @@ int FTI_Clean(int level, int group, int rank);
 
  **/
 /*-------------------------------------------------------------------------*/
-void FTI_Print(char *msg, int priority) {
-    if (priority >= FTI_Conf.verbosity)
-    {
-        if (msg != NULL)
-        {
-            switch(priority)
-            {
-                case FTI_EROR:
-                    fprintf(stderr, "[FTI Error - %06d] : %s : %s \n", FTI_Topo.myRank, msg, strerror(errno));
-                    break;
-                case FTI_WARN:
-                    fprintf(stdout, "[FTI Warning %06d] : %s \n", FTI_Topo.myRank, msg);
-                    break;
-                case FTI_INFO:
-                    if (FTI_Topo.splitRank == 0)
-                        fprintf(stdout, "[ FTI  Information ] : %s \n", msg);
-                    break;
-                case FTI_DBUG:
-                    fprintf(stdout, "[FTI Debug - %06d] : %s \n", FTI_Topo.myRank, msg);
-                    break;
-                default:
-                    break;
+void FTI_Print(char* msg, int priority)
+{
+    if (priority >= FTI_Conf.verbosity) {
+        if (msg != NULL) {
+            switch (priority) {
+            case FTI_EROR:
+                fprintf(stderr, "[FTI Error - %06d] : %s : %s \n", FTI_Topo.myRank, msg, strerror(errno));
+                break;
+            case FTI_WARN:
+                fprintf(stdout, "[FTI Warning %06d] : %s \n", FTI_Topo.myRank, msg);
+                break;
+            case FTI_INFO:
+                if (FTI_Topo.splitRank == 0)
+                    fprintf(stdout, "[ FTI  Information ] : %s \n", msg);
+                break;
+            case FTI_DBUG:
+                fprintf(stdout, "[FTI Debug - %06d] : %s \n", FTI_Topo.myRank, msg);
+                break;
+            default:
+                break;
             }
         }
     }
     fflush(stdout);
 }
-
 
 /*-------------------------------------------------------------------------*/
 /**
@@ -68,13 +63,14 @@ void FTI_Print(char *msg, int priority) {
 
  **/
 /*-------------------------------------------------------------------------*/
-int FTI_Try(int result, char* message) {
+int FTI_Try(int result, char* message)
+{
     char str[FTI_BUFS];
-    if (result ==  FTI_SCES || result == FTI_DONE)
-    {
+    if (result == FTI_SCES || result == FTI_DONE) {
         sprintf(str, "FTI succeeded to %s", message);
         FTI_Print(str, FTI_DBUG);
-    } else {
+    }
+    else {
         sprintf(str, "FTI failed to %s", message);
         FTI_Print(str, FTI_WARN);
         sprintf(str, "Error => %s", strerror(errno));
@@ -82,7 +78,6 @@ int FTI_Try(int result, char* message) {
     }
     return result;
 }
-
 
 /*-------------------------------------------------------------------------*/
 /**
@@ -94,10 +89,10 @@ int FTI_Try(int result, char* message) {
 
  **/
 /*-------------------------------------------------------------------------*/
-int FTI_InitBasicTypes(FTIT_dataset FTI_Data[FTI_BUFS]) {
+int FTI_InitBasicTypes(FTIT_dataset FTI_Data[FTI_BUFS])
+{
     int i;
-    for (i = 0; i < FTI_BUFS; i++)
-    {
+    for (i = 0; i < FTI_BUFS; i++) {
         FTI_Data[i].id = -1;
     }
     FTI_InitType(&FTI_CHAR, sizeof(char));
@@ -113,7 +108,6 @@ int FTI_InitBasicTypes(FTIT_dataset FTI_Data[FTI_BUFS]) {
     FTI_InitType(&FTI_LDBE, sizeof(long double));
     return FTI_SCES;
 }
-
 
 /*-------------------------------------------------------------------------*/
 /**
@@ -131,32 +125,31 @@ int FTI_InitBasicTypes(FTIT_dataset FTI_Data[FTI_BUFS]) {
 
 int FTI_RmDir(char path[FTI_BUFS], int flag)
 {
-    if (flag && (!access(path, R_OK)))
-    {
+    if (flag && (!access(path, R_OK))) {
         DIR* dp;
         char buf[FTI_BUFS], fn[FTI_BUFS], fil[FTI_BUFS];
         struct dirent* ep;
         dp = opendir(path);
         sprintf(buf, "Removing directory %s and its files.", path);
         FTI_Print(buf, FTI_DBUG);
-        if (dp != NULL)
-        {
-            while(ep = readdir(dp))
-            {
+        if (dp != NULL) {
+            while (ep = readdir(dp)) {
                 sprintf(fil, "%s", ep->d_name);
-                if ((strcmp(fil, ".") != 0) && (strcmp(fil, "..") != 0))
-                {
+                if ((strcmp(fil, ".") != 0) && (strcmp(fil, "..") != 0)) {
                     sprintf(fn, "%s/%s", path, fil);
                     sprintf(buf, "File %s will be removed.", fn);
                     FTI_Print(buf, FTI_DBUG);
-                    if (remove(fn) != 0) FTI_Print("Error removing target file.", FTI_EROR);
+                    if (remove(fn) != 0)
+                        FTI_Print("Error removing target file.", FTI_EROR);
                 }
             }
-        } else {
+        }
+        else {
             FTI_Print("Error with opendir.", FTI_EROR);
         }
         closedir(dp);
-        if (remove(path) != 0) FTI_Print("Error removing target directory.", FTI_EROR);
+        if (remove(path) != 0)
+            FTI_Print("Error removing target directory.", FTI_EROR);
     }
     return FTI_SCES;
 }
@@ -175,52 +168,44 @@ int FTI_RmDir(char path[FTI_BUFS], int flag)
 
  **/
 /*-------------------------------------------------------------------------*/
-int FTI_Clean(int level, int group, int rank) {
+int FTI_Clean(int level, int group, int rank)
+{
     char buf[FTI_BUFS];
     int nodeFlag, globalFlag = !FTI_Topo.splitRank;
-    nodeFlag = (((!FTI_Topo.amIaHead) && (FTI_Topo.nodeRank == 0)) || (FTI_Topo.amIaHead))? 1 : 0;
-    if (level == 0)
-    {
+    nodeFlag = (((!FTI_Topo.amIaHead) && (FTI_Topo.nodeRank == 0)) || (FTI_Topo.amIaHead)) ? 1 : 0;
+    if (level == 0) {
         FTI_RmDir(FTI_Conf.mTmpDir, globalFlag);
         FTI_RmDir(FTI_Conf.gTmpDir, globalFlag);
         FTI_RmDir(FTI_Conf.lTmpDir, nodeFlag);
     }
-    if (level >= 1)
-    { // Clean last checkpoint level 1
-	FTI_RmDir(FTI_Ckpt[1].metaDir, globalFlag);
-	FTI_RmDir(FTI_Ckpt[1].dir, nodeFlag);
+    if (level >= 1) { // Clean last checkpoint level 1
+        FTI_RmDir(FTI_Ckpt[1].metaDir, globalFlag);
+        FTI_RmDir(FTI_Ckpt[1].dir, nodeFlag);
     }
-    if (level >= 2)
-    { // Clean last checkpoint level 2
-	FTI_RmDir(FTI_Ckpt[2].metaDir, globalFlag);
-	FTI_RmDir(FTI_Ckpt[2].dir, nodeFlag);
+    if (level >= 2) { // Clean last checkpoint level 2
+        FTI_RmDir(FTI_Ckpt[2].metaDir, globalFlag);
+        FTI_RmDir(FTI_Ckpt[2].dir, nodeFlag);
     }
-    if (level >= 3)
-    { // Clean last checkpoint level 3
-	FTI_RmDir(FTI_Ckpt[3].metaDir, globalFlag);
-	FTI_RmDir(FTI_Ckpt[3].dir, nodeFlag);
+    if (level >= 3) { // Clean last checkpoint level 3
+        FTI_RmDir(FTI_Ckpt[3].metaDir, globalFlag);
+        FTI_RmDir(FTI_Ckpt[3].dir, nodeFlag);
     }
-    if (level == 4 || level == 5)
-    { // Clean last checkpoint level 4
-	FTI_RmDir(FTI_Ckpt[4].metaDir, globalFlag);
-	FTI_RmDir(FTI_Ckpt[4].dir, globalFlag);
+    if (level == 4 || level == 5) { // Clean last checkpoint level 4
+        FTI_RmDir(FTI_Ckpt[4].metaDir, globalFlag);
+        FTI_RmDir(FTI_Ckpt[4].dir, globalFlag);
         rmdir(FTI_Conf.gTmpDir);
     }
-    if (level == 5)
-    { // If it is the very last cleaning and we DO NOT keep the last checkpoint
+    if (level == 5) { // If it is the very last cleaning and we DO NOT keep the last checkpoint
         rmdir(FTI_Conf.lTmpDir);
         rmdir(FTI_Conf.localDir);
         rmdir(FTI_Conf.glbalDir);
-        snprintf(buf, FTI_BUFS, "%s/Topology.fti",FTI_Conf.metadDir);
+        snprintf(buf, FTI_BUFS, "%s/Topology.fti", FTI_Conf.metadDir);
         remove(buf);
         rmdir(FTI_Conf.metadDir);
     }
-    if (level == 6)
-    { // If it is the very last cleaning and we DO keep the last checkpoint
+    if (level == 6) { // If it is the very last cleaning and we DO keep the last checkpoint
         rmdir(FTI_Conf.lTmpDir);
         rmdir(FTI_Conf.localDir);
     }
     return FTI_SCES;
 }
-
-
