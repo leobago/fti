4e2078c4c7e25da0c7e6d4f2c1bd61bcefe7fc84
Karol <ksiero@man.poznan.pl>
2017-03-02 13:32:42 +0100

    Debug mode

--- test/addInArray/test.sh
+++ test/addInArray/test.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 printError () {
     case $1 in
-        "1") echo "	Error: Result not correct!"
-	     exit 1 ;;
-        "2") echo "	Error: Checkpoint failed!"
-             exit 1 ;;
-        "3") echo "	Error: Recovery failed!" 
-             exit 1 ;;
+        "1") echo "	Error: Result not correct!" ;;
+        "2") echo "	Error: Checkpoint failed!" ;;
+        "3") echo "	Error: Recovery failed!" ;;
 	*) echo "	$1"
     esac
 }
@@ -17,22 +14,19 @@ test () {
     if [ "$FLAG" != 0 ]
     then
         printError "$FLAG"
-        exit 1
+        #exit 1
     fi
     FLAG=$(sudo mpirun -n 8 ./addInArray config.fti $1 0)
     if [ "$FLAG" != 0 ]
     then
         printError "$FLAG"
-        exit 1
+        #exit 1
     fi
 }
 
 
 
 cd addInArray
-echo "	Including FTI..."
-C_INCLUDE_PATH=../../include/ #access from subfolder
-export C_INCLUDE_PATH
 echo "	Making..."
 make
 for i in ${@:2}
