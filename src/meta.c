9e9560b5a13410e9ef3d600c019365656d29ecf0
Karol <ksiero@man.poznan.pl>
2017-03-09 13:56:00 +0100

    Debug cnt (infinite loop)

--- src/meta.c
+++ src/meta.c
@@ -27,7 +27,7 @@ int FTI_GetMeta(FTIT_configuration* FTI_Conf, FTIT_execution* FTI_Exec,
                 unsigned long* fs, unsigned long* mfs, int group, int level)
 {
     dictionary* ini;
-    int res = -1, cnt = 1000;
+    int res = -1, cnt = 0;
     char mfn[FTI_BUFS], str[FTI_BUFS], *cfn;
     if (level == 0) {
         sprintf(mfn, "%s/sector%d-group%d.fti", FTI_Conf->mTmpDir, FTI_Topo->sectorID, group);
@@ -37,7 +37,17 @@ int FTI_GetMeta(FTIT_configuration* FTI_Conf, FTIT_execution* FTI_Exec,
     }
     sprintf(str, "Getting FTI metadata file (%s)...", mfn);
     FTI_Print(str, FTI_DBUG);
-    res = access(mfn, R_OK);
+    while (res != 0) {
+        usleep(100); //delay for accessing metadata file
+	sprintf(str, "Getting FTI metadata file (%s)... xx1:%d", mfn, cnt);
+        FTI_Print(str, FTI_DBUG);
+        res = access(mfn, R_OK);
+        cnt++;
+    }
+    if (cnt > 5) {
+	sprintf(str, "It took xx2:%d", cnt);
+        FTI_Print(str, FTI_DBUG);
+    }
     if (res != 0) {
         FTI_Print("FTI metadata file NOT accessible.", FTI_DBUG);
         return FTI_NSCS;
