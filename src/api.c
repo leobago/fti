1b00a6e1a00d2805f28f0e95b765fd3a1804ef5c
Karol <ksiero@man.poznan.pl>
2017-03-09 09:31:34 +0100

    Added 1s delay after Fflush2

--- src/api.c
+++ src/api.c
@@ -544,7 +544,7 @@ int FTI_Finalize()
     if (FTI_Conf.saveLastCkpt && FTI_Exec.ckptID > 0) {
         if (FTI_Exec.lastCkptLvel != 4) {
             FTI_Try(FTI_Flush(&FTI_Conf, &FTI_Exec, &FTI_Topo, FTI_Ckpt, FTI_Topo.groupID, FTI_Exec.lastCkptLvel), "save the last ckpt. in the PFS.");
-            uslepp(1000);
+            usleep(1000);
    	    MPI_Barrier(FTI_COMM_WORLD);
             if (FTI_Topo.splitRank == 0) {
                 if (access(FTI_Ckpt[4].dir, 0) == 0)
