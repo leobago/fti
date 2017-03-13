97cd30fbc0a7f3cfb67bff61fe7c7d9583ed4bb0
Karol <ksiero@man.poznan.pl>
2017-03-13 09:55:50 +0100

    Running all tests

--- test/tests.sh
+++ test/tests.sh
@@ -49,21 +49,21 @@ startTest () {
 	esac
 }
 runAllConfiguration() {
-	#startTest addInArray configH0I1Silent.fti $1 1 2 3 4
-	#startTest addInArray configH1I1Silent.fti $1 1 2 3 4
-	#startTest addInArray configH1I0Silent.fti $1 1 2 3 4
-	#startTest nodeFlag configH0I1.fti $1
-	#startTest nodeFlag configH1I1.fti $1
-	#startTest nodeFlag configH1I0.fti $1
-	#startTest tokenRing configH0I1.fti $1 1 2 3 4
-	#startTest tokenRing configH1I1.fti $1 1 2 3 4
-	#startTest tokenRing configH1I0.fti $1 1 2 3 4
-	#startTest heatdis configH0I1Silent.fti $1
-	#startTest heatdis configH1I1Silent.fti $1
-	#startTest heatdis configH1I0Silent.fti $1
-	startTest lvlsRecovery configH0I1.fti $1 1 2 3 4
-	startTest lvlsRecovery configH1I1.fti $1 1 2 3 4
-	startTest lvlsRecovery configH1I0.fti $1 1 2 3 4
+	startTest addInArray configH0I1Silent.fti $1 1 2 3 4
+	startTest addInArray configH1I1Silent.fti $1 1 2 3 4
+	startTest addInArray configH1I0Silent.fti $1 1 2 3 4
+	startTest nodeFlag configH0I1.fti $1
+	startTest nodeFlag configH1I1.fti $1
+	startTest nodeFlag configH1I0.fti $1
+	startTest tokenRing configH0I1Silent.fti $1 1 2 3 4
+	startTest tokenRing configH1I1Silent.fti $1 1 2 3 4
+	startTest tokenRing configH1I0Silent.fti $1 1 2 3 4
+	startTest heatdis configH0I1Silent.fti $1
+	startTest heatdis configH1I1Silent.fti $1
+	startTest heatdis configH1I0Silent.fti $1
+	startTest lvlsRecovery configH0I1Silent.fti $1 1 2 3 4
+	startTest lvlsRecovery configH1I1Silent.fti $1 1 2 3 4
+	startTest lvlsRecovery configH1I0Silent.fti $1 1 2 3 4
 }
 
 cd test
