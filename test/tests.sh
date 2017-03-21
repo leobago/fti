12a144b172dbb5d6e74a0f3e5dd835f5a8ee2bf1
Karol <ksiero@man.poznan.pl>
2017-03-21 10:01:58 +0100

    Refactored tests.sh file

--- test/tests.sh
+++ test/tests.sh
@@ -14,32 +14,31 @@ startTest () {
 		check $?
 }
 runAllConfiguration() {
-	startTest addInArray configH0I1Silent.fti $1 1 2 3 4
-	startTest addInArray configH1I1Silent.fti $1 1 2 3 4
-	startTest addInArray configH1I0Silent.fti $1 1 2 3 4
-	startTest nodeFlag configH0I1.fti $1
-	startTest nodeFlag configH1I1.fti $1
-	startTest nodeFlag configH1I0.fti $1
-	startTest tokenRing configH0I1Silent.fti $1 1 2 3 4
-	startTest tokenRing configH1I1Silent.fti $1 1 2 3 4
-	startTest tokenRing configH1I0Silent.fti $1 1 2 3 4
-	startTest diffSizes configH1I0Silent.fti 16 1 2 3 4
-	startTest diffSizes configH1I1Silent.fti 16 1 2 3 4
-	startTest diffSizes configH0I1Silent.fti 16 1 2 3 4
-	startTest lvlsRecovery configH0I1Silent.fti $1 1 2 3 4
-	startTest lvlsRecovery configH1I1Silent.fti $1 1 2 3 4
-	startTest lvlsRecovery configH1I0Silent.fti $1 1 2 3 4
-	startTest heatdis configH0I1Silent.fti $1
-	startTest heatdis configH1I1Silent.fti $1
-	startTest heatdis configH1I0Silent.fti $1
+	for i in {0..2}
+	do
+		startTest addInArray ${silentConfigs[$i]} $1 1 2 3 4
+		startTest nodeFlag ${configs[$i]} $1
+		startTest tokenRing ${silentConfigs[$i]} $1 1 2 3 4
+		startTest diffSizes ${silentConfigs[$i]} $1 1 2 3 4
+		startTest lvlsRecovery ${silentConfigs[$i]} $1 1 2 3 4
+	done
+	#slow test at the end
+	for i in {0..2}
+	do
+		startTest heatdis ${silentConfigs[$i]} $1
+	done
 }
 
 cd test
-#Pattern: startTest testName configFile procNo args(ex. checkpoint levels)
-#----------------------
-#-- Write tests here --
+#--------Pattern: startTest testName configFile procNo args(ex. checkpoint levels)-------
+#----------------------------------------------------------------------------------------
+#-------------------------------- Write tests here --------------------------------------
 
-runAllConfiguration 16
+	configs=(configH0I1.fti configH1I1.fti configH1I0.fti)
+	silentConfigs=(configH0I1Silent.fti configH1I1Silent.fti configH1I0Silent.fti)
 
-#----------------------
+	#runs all configuration with 16 processes
+	runAllConfiguration 16
+
+#----------------------------------------------------------------------------------------
 cd ..
