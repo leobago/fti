a720af17b86cd4519e33f08885c4e1b89eded334
Karol <ksiero@man.poznan.pl>
2017-03-27 10:27:31 +0200

    Debug

--- test/tests.sh
+++ test/tests.sh
@@ -16,17 +16,17 @@ startTest () {
 runAllConfiguration() {
 	for i in {0..2}
 	do
-		startTest addInArray ${silentConfigs[$i]} $1 1 2 3 4
-		startTest nodeFlag ${configs[$i]} $1
-		startTest tokenRing ${silentConfigs[$i]} $1 1 2 3 4
-		startTest diffSizes ${silentConfigs[$i]} $1 1 2 3 4
+		#startTest addInArray ${silentConfigs[$i]} $1 1 2 3 4
+		#startTest nodeFlag ${configs[$i]} $1
+		#startTest tokenRing ${silentConfigs[$i]} $1 1 2 3 4
+		#startTest diffSizes ${silentConfigs[$i]} $1 1 2 3 4
 		startTest lvlsRecovery ${silentConfigs[$i]} $1 1 2 3 4
 	done
 	#slow test at the end
-	for i in {0..2}
-	do
-		startTest heatdis ${silentConfigs[$i]} $1
-	done
+	#for i in {0..2}
+	#do
+	#	startTest heatdis ${silentConfigs[$i]} $1
+	#done
 }
 
 cd test
@@ -35,7 +35,7 @@ cd test
 #-------------------------------- Write tests here --------------------------------------
 
 	configs=(configH0I1.fti configH1I1.fti configH1I0.fti)
-	silentConfigs=(configH0I1Silent.fti configH1I1Silent.fti configH1I0Silent.fti)
+	silentConfigs=(configH0I1.fti configH1I1.fti configH1I0.fti) #(configH0I1Silent.fti configH1I1Silent.fti configH1I0Silent.fti)
 
 	#runs all configuration with 16 processes
 	runAllConfiguration 16
