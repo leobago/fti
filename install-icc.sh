5864bac8475dee6c6f925463f74945528e14b446
Karol <ksiero@man.poznan.pl>
2017-03-28 12:22:15 +0200

    Added icc 24

--- install-icc.sh
+++ install-icc.sh
@@ -12,20 +12,25 @@
 # See <https://creativecommons.org/publicdomain/zero/1.0/> for
 # details.
 
-COMPONENTS_COMPILER_COMMON="intel-comp-l-all-vars__noarch;intel-comp-l-all-common;intel-comp-l-ps-common;intel-comp-l-all-devel;intel-comp-l-ps-ss-devel;intel-comp-l-ps-ss-wrapper;intel-comp-l-all-devel;intel-comp-l-ps-devel__x86_64;intel-comp-l-ps-ss-devel__x86_64"
-COMPONENTS_ICC="intel-icc-l-all__x86_64;intel-icc-l-ps-ss__x86_64;intel-icc-l-all-vars__noarch;intel-icc-l-all-common__noarch;intel-icc-l-ps-common__noarch;intel-icc-l-all-devel__x86_64;intel-icc-l-ps-devel__x86_64;intel-icc-l-ps-ss-devel__x86_64;${COMPONENTS_COMPILER_COMMON}"
-COMPONENTS_MPI="intel-mpi-rt-core__x86_64;intel-mpi-rt-mic__x86_64;intel-mpi-sdk-core__x86_64;intel-mpi-sdk-mic__x86_64;intel-mpi-psxe__x86_64;intel-mpi-rt-psxe__x86_64"
+# Tools - Not very useful in non-interactive mode.
 COMPONENTS_VTUNE="intel-vtune-amplifier-xe-2016-cli__x86_64;intel-vtune-amplifier-xe-2016-common__noarch;intel-vtune-amplifier-xe-2016-cli-common__noarch;intel-vtune-amplifier-xe-2016-collector-64linux__x86_64;intel-vtune-amplifier-xe-2016-sep__noarch;intel-vtune-amplifier-xe-2016-gui-common__noarch;intel-vtune-amplifier-xe-2016-gui__x86_64;intel-vtune-amplifier-xe-2016-common-pset__noarch"
 COMPONENTS_INSPECTOR="intel-inspector-xe-2016-cli__x86_64;intel-inspector-xe-2016-cli-common__noarch;intel-inspector-xe-2016-gui-common__noarch;intel-inspector-xe-2016-gui__x86_64;intel-inspector-xe-2016-cli-pset__noarch"
 COMPONENTS_ADVISOR="intel-advisor-xe-2016-cli__x86_64;intel-advisor-xe-2016-cli-common__noarch;intel-advisor-xe-2016-gui-common__noarch;intel-advisor-xe-2016-gui__x86_64;intel-advisor-xe-2016-cli-pset__noarch"
-COMPONENTS_OPENMP="intel-openmp-l-all__x86_64;intel-openmp-l-ps-mic__x86_64;intel-openmp-l-ps__x86_64;intel-openmp-l-ps-ss__x86_64;intel-openmp-l-all-devel__x86_64;intel-openmp-l-ps-mic-devel__x86_64;intel-openmp-l-ps-devel__x86_64;intel-openmp-l-ps-ss-devel__x86_64"
+COMPONENTS_GDB="intel-gdb-gt__x86_64;intel-gdb-gt-src__noarch;intel-gdb-gt-libelfdwarf__x86_64;intel-gdb-gt-devel__x86_64;intel-gdb-gt-common__noarch;intel-gdb-ps-cdt__x86_64;intel-gdb-ps-cdt-source__x86_64;intel-gdb-ps-mic__x86_64;intel-gdb-ps-mpm__x86_64;intel-gdb__x86_64;intel-gdb-source__noarch;intel-gdb-python-source__noarch;intel-gdb-common__noarch;intel-gdb-ps-common__noarch"
+
+# Parallel libraries
+COMPONENTS_MPI="intel-mpi-rt-core__x86_64;intel-mpi-rt-mic__x86_64;intel-mpi-sdk-core__x86_64;intel-mpi-sdk-mic__x86_64;intel-mpi-psxe__x86_64;intel-mpi-rt-psxe__x86_64"
 COMPONENTS_TBB="intel-tbb-libs__noarch;intel-mkl-ps-tbb__x86_64;intel-mkl-ps-tbb-devel__x86_64;intel-mkl-ps-tbb-mic__x86_64;intel-mkl-ps-tbb-mic-devel__x86_64;intel-tbb-source__noarch;intel-tbb-devel__noarch;intel-tbb-common__noarch;intel-tbb-ps-common__noarch;intel-tbb-psxe__noarch"
-COMPONENTS_IFORT="intel-ifort-l-ps__x86_64;intel-ifort-l-ps-vars__noarch;intel-ifort-l-ps-common__noarch;intel-ifort-l-ps-devel__x86_64;${COMPONENTS_COMPILER_COMMON}"
 COMPONENTS_MKL="intel-mkl__x86_64;intel-mkl-ps__x86_64;intel-mkl-common__noarch;intel-mkl-ps-common__noarch;intel-mkl-devel__x86_64;intel-mkl-ps-mic-devel__x86_64;intel-mkl-ps-f95-devel__x86_64;intel-mkl-gnu-devel__x86_64;intel-mkl-ps-gnu-devel__x86_64;intel-mkl-ps-pgi-devel__x86_64;intel-mkl-sp2dp-devel__x86_64;intel-mkl-ps-cluster-devel__x86_64;intel-mkl-ps-cluster-common__noarch;intel-mkl-ps-f95-common__noarch;intel-mkl-ps-cluster__x86_64;intel-mkl-gnu__x86_64;intel-mkl-ps-gnu__x86_64;intel-mkl-ps-pgi__x86_64;intel-mkl-sp2dp__x86_64;intel-mkl-ps-mic__x86_64;intel-mkl-ps-tbb__x86_64;intel-mkl-ps-tbb-devel__x86_64;intel-mkl-ps-tbb-mic__x86_64;intel-mkl-ps-tbb-mic-devel__x86_64;intel-mkl-psxe__noarch"
 COMPONENTS_IPP="intel-ipp-l-common__noarch;intel-ipp-l-ps-common__noarch;intel-ipp-l-st__x86_64;intel-ipp-l-mt__x86_64;intel-ipp-l-st-devel__x86_64;intel-ipp-l-ps-st-devel__x86_64;intel-ipp-l-mt-devel__x86_64;intel-ipp-psxe__noarch"
 COMPONENTS_IPP_CRYPTO="intel-crypto-ipp-st-devel__x86_64;intel-crypto-ipp-ps-st-devel__x86_64;intel-crypto-ipp-st__x86_64;intel-crypto-ipp-mt-devel__x86_64;intel-crypto-ipp-mt__x86_64;intel-crypto-ipp-ss-st-devel__x86_64;intel-crypto-ipp-common__noarch"
-COMPONENTS_GDB="intel-gdb-gt__x86_64;intel-gdb-gt-src__noarch;intel-gdb-gt-libelfdwarf__x86_64;intel-gdb-gt-devel__x86_64;intel-gdb-gt-common__noarch;intel-gdb-ps-cdt__x86_64;intel-gdb-ps-cdt-source__x86_64;intel-gdb-ps-mic__x86_64;intel-gdb-ps-mpm__x86_64;intel-gdb__x86_64;intel-gdb-source__noarch;intel-gdb-python-source__noarch;intel-gdb-common__noarch;intel-gdb-ps-common__noarch"
 COMPONENTS_DAAL="intel-daal__x86_64;intel-daal-common__noarch"
+
+# Compilers
+COMPONENTS_OPENMP="intel-openmp-l-all__x86_64;intel-openmp-l-ps-mic__x86_64;intel-openmp-l-ps__x86_64;intel-openmp-l-ps-ss__x86_64;intel-openmp-l-all-devel__x86_64;intel-openmp-l-ps-mic-devel__x86_64;intel-openmp-l-ps-devel__x86_64;intel-openmp-l-ps-ss-devel__x86_64"
+COMPONENTS_COMPILER_COMMON="intel-comp-l-all-vars__noarch;intel-comp-l-all-common;intel-comp-l-ps-common;intel-comp-l-all-devel;intel-comp-l-ps-ss-devel;intel-comp-l-ps-ss-wrapper;intel-comp-l-all-devel;intel-comp-l-ps-devel__x86_64;intel-comp-l-ps-ss-devel__x86_64;intel-psf-intel__x86_64;intel-icsxe-pset;intel-ipsf__noarch;intel-ccompxe__noarch;${COMPONENTS_OPENMP}"
+COMPONENTS_IFORT="intel-ifort-l-ps__x86_64;intel-ifort-l-ps-vars__noarch;intel-ifort-l-ps-common__noarch;intel-ifort-l-ps-devel__x86_64;${COMPONENTS_COMPILER_COMMON}"
+COMPONENTS_ICC="intel-icc-l-all__x86_64;intel-icc-l-ps-ss__x86_64;intel-icc-l-all-vars__noarch;intel-icc-l-all-common__noarch;intel-icc-l-ps-common__noarch;intel-icc-l-all-devel__x86_64;intel-icc-l-ps-devel__x86_64;intel-icc-l-ps-ss-devel__x86_64;${COMPONENTS_COMPILER_COMMON}"
 
 DESTINATION="${HOME}/intel"
 TEMPORARY_FILES="/tmp"
@@ -34,9 +39,10 @@ COMPONENTS=""
 
 add_components() {
     if [ ! -z "${COMPONENTS}" ]; then
-	COMPONENTS="${COMPONENTS};"
+	COMPONENTS="${COMPONENTS};$1"
+    else
+	COMPONENTS="$1"
     fi
-    COMPONENTS="${COMPONENTS}$1"
 }
 
 while [ $# != 0 ]; do
@@ -54,7 +60,6 @@ while [ $# != 0 ]; do
 	    for component in $1; do
 		case "$component" in
 		    "icc")
-			# Do nothing, we always install icc
 			add_components "${COMPONENTS_ICC}"
 		        ;;
 		    "mpi")
@@ -69,9 +74,6 @@ while [ $# != 0 ]; do
 		    "advisor")
 			add_components "${COMPONENTS_ADVISOR}"
 			;;
-		    "openmp")
-			add_components "${COMPONENTS_OPENMP}"
-			;;
 		    "tbb")
 			add_components "${COMPONENTS_TBB}"
 			;;
@@ -81,6 +83,9 @@ while [ $# != 0 ]; do
 		    "mkl")
 			add_components "${COMPONENTS_MKL}"
 			;;
+		    "openmp")
+			# Noop, here for compatability.  OpenMP is installed with icc and ifort now.
+			;;
 		    "ipp")
 			add_components "${COMPONENTS_IPP}"
 			;;
@@ -89,6 +94,9 @@ while [ $# != 0 ]; do
 			;;
 		    "gdb")
 			add_components "${COMPONENTS_GDB}"
+			;;
+		    "daal")
+			add_components "${COMPONENTS_DAAL}"
 			;;
 		    *)
 			echo "Unknown component '$component'"
@@ -107,11 +115,12 @@ while [ $# != 0 ]; do
 done
 
 if [ -z "${COMPONENTS}" ]; then
-    COMPONENTS="${COMPONENTS_ICC};intel-psf-intel__x86_64;intel-icsxe-pset;intel-ipsf__noarch;intel-ccompxe__noarch"
+    COMPONENTS="${COMPONENTS_ICC}"
 fi
 
-INSTALLER="${TEMPORARY_FILES}/parallel_studio_xe_2016_online.sh"
-INSTALLER_URL="http://registrationcenter-download.intel.com/akdlm/irc_nas/7997/parallel_studio_xe_2016_online.sh"
+INSTALLER_SCRIPT="parallel_studio_xe_2016_update3_online.sh"
+INSTALLER="${TEMPORARY_FILES}/${INSTALLER_SCRIPT}"
+INSTALLER_URL="http://registrationcenter-download.intel.com/akdlm/irc_nas/9061/${INSTALLER_SCRIPT}"
 SILENT_CFG="${TEMPORARY_FILES}/silent.cfg"
 SUCCESS_INDICATOR="${TEMPORARY_FILES}/icc-travis-success"
 
@@ -139,34 +148,68 @@ echo "PSET_INSTALL_DIR=${DESTINATION}" >> "${SILENT_CFG}"
 echo "CONTINUE_WITH_INSTALLDIR_OVERWRITE=yes" >> "${SILENT_CFG}"
 echo "COMPONENTS=${COMPONENTS}" >> "${SILENT_CFG}"
 echo "PSET_MODE=install" >> "${SILENT_CFG}"
-#echo "ACTIVATION_SERIAL_NUMBER=${INTEL_SERIAL_NUMBER}" >> "${SILENT_CFG}"
-#echo "ACTIVATION_TYPE=serial_number" >> "${SILENT_CFG}"
-#echo "ACTIVATION_LICENSE_FILE=${INTEL_LICENSE_FILE}" >> "${SILENT_CFG}"
-echo "ACTIVATION_TYPE=trial_lic" >> "${SILENT_CFG}"
 echo "PHONEHOME_SEND_USAGE_DATA=${PHONE_INTEL}" >> "${SILENT_CFG}"
+if [ "x" != "x${INTEL_SERIAL_NUMBER}" ]; then
+    echo "ACTIVATION_SERIAL_NUMBER=${INTEL_SERIAL_NUMBER}" >> "${SILENT_CFG}"
+    echo "ACTIVATION_TYPE=serial_number" >> "${SILENT_CFG}"
+else
+    echo "ACTIVATION_TYPE=trial_lic" >> "${SILENT_CFG}"
+fi
 
-cat ${SILENT_CFG}
-
-("${INSTALLER}" \
-    -t "${TEMPORARY_FILES}" \
-    -s "${SILENT_CFG}" \
-    --cli-mode \
-    --user-mode && \
-touch "${SUCCESS_INDICATOR}") &
-
-# So Travis doesn't die in case of a long download/installation.
-elapsed=0;
-while kill -0 $! 2>/dev/null; do
-    sleep 1
-    elapsed=$(expr $elapsed + 1)
-    if [ $(expr $elapsed % 60) -eq 0 ]; then
-	echo "Still running... (about $(expr $elapsed / 60) minutes so far)."
+attempt=1;
+while [ $attempt -le 3 ]; do
+    if [ ! -e "${TEMPORARY_FILES}/parallel-studio-install-data" ]; then
+	mkdir -p "${TEMPORARY_FILES}/parallel-studio-install-data" || (sudo mkdir -p "${TEMPORARY_FILES}/parallel-studio-install-data" && sudo chown -R "${USER}:${USER}" "${TEMPORARY_FILES}")
     fi
+
+    ("${INSTALLER}" \
+	 -t "${TEMPORARY_FILES}/parallel-studio-install-data" \
+	 -s "${SILENT_CFG}" \
+	 --cli-mode \
+	 --user-mode && \
+	 touch "${SUCCESS_INDICATOR}") &
+
+    # So Travis doesn't die in case of a long download/installation.
+    #
+    # NOTE: a watched script never terminates.
+    elapsed=0;
+    while kill -0 $! 2>/dev/null; do
+	sleep 1
+	elapsed=$(expr $elapsed + 1)
+	if [ $(expr $elapsed % 60) -eq 0 ]; then
+	    mins_elapsed=$(expr $elapsed / 60)
+	    if [ $mins_elapsed = 1 ]; then
+		minute_string="minute"
+	    else
+		minute_string="minutes"
+	    fi
+	    echo "Still running... (about $(expr $elapsed / 60) ${minute_string} so far)."
+	fi
+    done
+
+    if [ ! -e "${SUCCESS_INDICATOR}" ]; then
+	echo "Installation failed."
+	exit 1
+    fi
+
+    if [ ! -e "${DESTINATION}/bin/compilervars.sh" ]; then
+	# Sometimes the installer returns successfully without actually
+	# installing anything.  Let's try again…
+	echo "Installation attempt #${attempt} completed, but unable to find compilervars.sh."
+	find "${DESTINATION}"
+    else
+	break
+    fi
+
+    rm -vrf "${TEMPORARY_FILES}/parallel-studio-install-data"
+    echo "Trying again..."
+
+    attempt=$(expr $attempt + 1)
 done
 
-if [ ! -e "${SUCCESS_INDICATOR}" ]; then
-    echo "Installation failed."
-    exit 1
+if [ ! -e "${DESTINATION}/bin/compilervars.sh" ]; then
+	echo "Installation failed."
+	exit 1
 fi
 
 # Apparently the installer drops the license file in a location it
