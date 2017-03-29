a971eed65c844dd033b4be696841ac5ced40d986
Karol <ksiero@man.poznan.pl>
2017-03-29 08:49:58 +0200

    Added icc 47

--- test/addInArray/test.sh
+++ test/addInArray/test.sh
@@ -19,7 +19,7 @@ test () {
 cd addInArray
 echo "	Making..."
 make
-sudo mpirun -n 16 ./testMPI
+sudo ./testMPI
 for i in ${@:3}
 do
 	echo "	Testing L"$i"..."
--- test/addInArray/testMPI.c
+++ test/addInArray/testMPI.c
@@ -3,8 +3,8 @@
 
 int main(int argc, char *argv[])
 {
-    MPI_Init(&argc, &argv);
-
+    int provided = MPI_Init_thread(&argc, &argv, 16);
+	printf("%d: Provided = %d\n", provided);
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
