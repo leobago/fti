34f2f9dd80a1ce7229279ccd2dfad83b203a003a
Karol <ksiero@man.poznan.pl>
2017-03-29 13:37:37 +0200

    Debug LvlsRecovery10

--- src/postckpt.c
+++ src/postckpt.c
@@ -107,7 +107,7 @@ int FTI_Ptner(FTIT_configuration* FTI_Conf, FTIT_execution* FTI_Exec,
 
             return FTI_NSCS;
         }
-
+        MPI_Buffer_attach(malloc(500004), 500004);
         MPI_Ibsend(blBuf1, bytes, MPI_CHAR, dest, FTI_Conf->tag, FTI_Exec->groupComm, &reqSend);
         MPI_Irecv(blBuf2, FTI_Conf->blockSize, MPI_CHAR, src, FTI_Conf->tag, FTI_Exec->groupComm, &reqRecv);
         FTI_Print("MPI_Waiting recv...", FTI_DBUG);
