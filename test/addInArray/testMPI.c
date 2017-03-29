90251247ae818d87ce4325666ad4f248706f7a75
Karol <ksiero@man.poznan.pl>
2017-03-29 08:58:50 +0200

    Added icc 49

--- test/addInArray/testMPI.c
+++ test/addInArray/testMPI.c
@@ -3,7 +3,8 @@
 
 int main(int argc, char *argv[])
 {
-    int provided = MPI_Init_thread(&argc, &argv, 16);
+    int provided = -1;
+	MPI_Init_thread(&argc, &argv, 16, &provided);
 	printf("Provided = %d\n", provided);
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
