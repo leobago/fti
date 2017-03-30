add4396de9116036f4f0aab119782bc3b548666e
Karol <ksiero@man.poznan.pl>
2017-03-30 11:21:42 +0200

    Clean-up

--- install-icc.sh
+++ install-icc.sh
@@ -248,3 +248,9 @@ echo "Looking for libimf..."
 find "${DESTINATION}" -name 'libimf.so*'
 
 echo "Installation successful!"
+
+source ~/.bashrc
+source /opt/intel/bin/compilervars.sh intel64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/intel/ism/bin/intel64
+CC=icc
+CXX=icpc
