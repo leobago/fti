e9a1265327254d1b1ddc59a91cf7cb59ee2ee61c
Karol <ksiero@man.poznan.pl>
2017-04-03 16:24:57 +0200

    Fixed Block_size>64KB

--- before-script-icc.sh
+++ before-script-icc.sh
@@ -1,5 +1,6 @@
 source ~/.bashrc
 source /opt/intel/bin/compilervars.sh intel64
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/intel/ism/bin/intel64
+export I_MPI_SHM_LMT=shm #https://software.intel.com/en-us/forums/intel-clusters-and-hpc-technology/topic/610561
 CC=icc
 CXX=icpc
