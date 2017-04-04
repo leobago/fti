ebb70c868dab73fd0ccd3b94892b79c3d02fc6f1
Karol <ksiero@man.poznan.pl>
2017-04-04 12:59:40 +0200

    Added pgi11

--- before-script-pgi.sh
+++ before-script-pgi.sh
@@ -5,3 +5,4 @@ FC=pgfortran
 export PATH=$PATH:/opt/pgi/linux86-64/2016/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/pgi/linux86-64/2016/lib
 export MPI_INCLUDE_PATH=/opt/pgi/linux86-64/2016/mpi/openmpi/include
+export CPATH=$CPATH:/opt/pgi/linux86-64/2016/include
