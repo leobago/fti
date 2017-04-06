80bb05ba476e9c1972c2d79c01c4a52b74e0ea0a
Karol <ksiero@man.poznan.pl>
2017-04-06 09:59:00 +0200

    Added pgi33

--- before-script-pgi.sh
+++ before-script-pgi.sh
@@ -1,6 +1,7 @@
 source ~/.bashrc
 CC=pgcc
-#CXX=pgcpp
+F90=pgfortran
+BPP_COMPILER_ID=PGI
 #FC=pgfortran
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/pgi/linux86-64/16.10/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lib
