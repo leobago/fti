0f4521a898a0122d95cf863cd47512f7cc4cf582
Karol <ksiero@man.poznan.pl>
2017-04-06 14:34:21 +0200

    Added pgi64

--- before-script-pgi.sh
+++ before-script-pgi.sh
@@ -7,5 +7,3 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/pgi/linux86-64/16.10/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/pgi/linux86-64/16.10/mpi/openmpi/lib
 export PATH=/opt/pgi/linux86-64/16.10/bin:$PATH
 export PATH=/opt/pgi/linux86-64/16.10/mpi/openmpi/bin:$PATH
-export MPI_Fortran_INCLUDE_PATH=/opt/pgi/linux86-64/16.10/mpi/openmpi/include
-export MPI_C_INCLUDE_PATH=/opt/pgi/linux86-64/16.10/mpi/openmpi/include
