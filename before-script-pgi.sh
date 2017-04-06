34e030f6cd92d6c29542e3fe3e31b1727f02749a
Karol <ksiero@man.poznan.pl>
2017-04-06 14:53:54 +0200

    Added pgi65

--- before-script-pgi.sh
+++ before-script-pgi.sh
@@ -1,9 +1,8 @@
 source ~/.bashrc
 CC=pgcc
 F90=pgfortran
-BPP_COMPILER_ID=PGI
 #FC=pgfortran
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/pgi/linux86-64/16.10/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/pgi/linux86-64/16.10/mpi/openmpi/lib
 export PATH=/opt/pgi/linux86-64/16.10/bin:$PATH
-export PATH=/opt/pgi/linux86-64/16.10/mpi/openmpi/bin:$PATH
+#export PATH=/opt/pgi/linux86-64/16.10/mpi/openmpi/bin:$PATH
