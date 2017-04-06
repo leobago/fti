d7bc19ddf5f7a6659ae064df4173cbe226316fb5
Karol <ksiero@man.poznan.pl>
2017-04-06 09:00:38 +0200

    Added pgi28

--- before-script-pgi.sh
+++ before-script-pgi.sh
@@ -2,6 +2,6 @@ source ~/.bashrc
 CC=pgcc
 #CXX=pgcpp
 #FC=pgfortran
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/pgi/linux86-64/2016/lib
-#export MPI_Fortran_INCLUDE_PATH=/opt/pgi/linux86-64/2016/mpi/openmpi/include
-#export MPI_C_INCLUDE_PATH=/opt/pgi/linux86-64/2016/mpi/openmpi/include
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/pgi/linux86-64/16.10/lib
+#export MPI_Fortran_INCLUDE_PATH=/opt/pgi/linux86-64/16.10/mpi/openmpi/include
+#export MPI_C_INCLUDE_PATH=/opt/pgi/linux86-64/16.10/mpi/openmpi/include
