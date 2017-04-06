a3ec6944718822afe1a973f54f2d1c19764ee7dc
Karol <ksiero@man.poznan.pl>
2017-04-06 13:13:56 +0200

    Added pgi55

--- before-script-pgi.sh
+++ before-script-pgi.sh
@@ -5,5 +5,5 @@ BPP_COMPILER_ID=PGI
 #FC=pgfortran
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/pgi/linux86-64/16.10/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/pgi/linux86-64/16.10/mpi/openmpi/lib
-export MPI_Fortran_LIBRARIES=/opt/pgi/linux86-64/16.10/mpi/openmpi/lib
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib
+export MPI_Fortran_INCLUDE_PATH=/opt/pgi/linux86-64/16.10/mpi/openmpi/include
+export MPI_C_INCLUDE_PATH=/opt/pgi/linux86-64/16.10/mpi/openmpi/include
