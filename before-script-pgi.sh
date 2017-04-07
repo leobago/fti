93c3ebf29f5038eb9379b25d26a670b26d91980b
Karol <ksiero@man.poznan.pl>
2017-04-07 10:57:14 +0200

    Added pgi85

--- before-script-pgi.sh
+++ before-script-pgi.sh
@@ -8,4 +8,5 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib
 export PATH=/usr/bin:$PATH
 export PATH=/opt/pgi/linux86-64/16.10/bin:$PATH
 export C_INCLUDE_PATH=/usr/lib/openmpi/include
+export FORTRAN_INCLUDE_PATH=/usr/lib/openmpi/include
 #export PATH=/opt/pgi/linux86-64/16.10/mpi/openmpi/bin:$PATH
