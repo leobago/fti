a33f1531b39c2613d17c8676ede2b2a5b62f669e
Karol <ksiero@man.poznan.pl>
2017-04-07 14:20:58 +0200

    Pgi test

--- before-script-pgi.sh
+++ before-script-pgi.sh
@@ -1,7 +1,16 @@
+wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-4.0-2.0.0.1/MLNX_OFED_LINUX-4.0-2.0.0.1-ubuntu14.04-x86_64.tgz
+gunzip < MLNX_OFED_LINUX-4.0-2.0.0.1-ubuntu14.04-x86_64.tgz | tar xvf -
+cd MLNX_OFED_LINUX-4.0-2.0.0.1-ubuntu14.04-x86_64
+yes | sudo ./mlnxofedinstall
+cd ..
+./install-pgi.sh --mpi
+sudo apt-get install -y libibverbs-dev librdmacm-dev
+
 source ~/.bashrc
 CC=pgcc
+CXX=pgc++
 F90=pgfortran
-#FC=pgfortran
+FC=pgfortran
 export PGI=/home/travis/pgi
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/travis/pgi/linux86-64/16.10/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/travis/pgi/linux86-64/16.10/mpi/openmpi/lib
