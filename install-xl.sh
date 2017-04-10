112d924b1efc9127a964c85737048d3bafa7cc41
Karol <ksiero@man.poznan.pl>
2017-04-10 09:57:36 +0200

    Changed to dpkg
    Former-commit-id: 443d7dd379170bc159fbff9160e88efab8e1afc9

--- install-xl.sh
+++ install-xl.sh
@@ -2,12 +2,16 @@
 #echo "deb http://public.dhe.ibm.com/software/server/POWER/Linux/xl-compiler/eval/ppc64le/ubuntu/ trusty main" | sudo tee /etc/apt/sources.list.d/ibm-xl-compiler-eval.list
 
 #sudo apt-get update -qq
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4B19F6F50761C815
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4B19F6F50761C815
 
-echo "deb http://public.dhe.ibm.com/software/server/POWER/Linux/rte/xlcpp/le/ubuntu $(lsb_release -s -c) main" | sudo tee -a /etc/apt/sources.list.d/ibm-xlcpp-rte.list
+#echo "http://public.dhe.ibm.com/software/server/POWER/Linux/rte/xlcpp/le/ubuntu/dists/xenial/main" | sudo tee -a /etc/apt/sources.list.d/ibm-xlcpp-rte.list
+wget ftp://public.dhe.ibm.com/software/server/POWER/Linux/rte/xlcpp/le/ubuntu/dists/xenial/main/binary-ppc64el/libxlc*
 
-sudo apt-get update
-sudo apt-get install libxlc
+sudo dpkg -iG *.deb 
+#sudo apt-get update
+#sudo apt-get install libxlc
+
+/usr/vacpp/bin/xlC -qversion
 
 ls -l /opt/ibm
 ls -l /opt/ibm/xlC
