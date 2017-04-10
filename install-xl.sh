43982128246ab972d4ef7a7aa9880957f777c0cc
Karol <ksiero@man.poznan.pl>
2017-04-10 09:32:09 +0200

    Changed to dpkg
    Former-commit-id: c82aa90aed69bb52bbf152c9ebf3e2d906060dfb

--- install-xl.sh
+++ install-xl.sh
@@ -1,9 +1,16 @@
-wget -q http://public.dhe.ibm.com/software/server/POWER/Linux/xl-compiler/eval/ppc64le/ubuntu/public.gpg -O- | sudo apt-key add -
-echo "deb http://public.dhe.ibm.com/software/server/POWER/Linux/xl-compiler/eval/ppc64le/ubuntu/ trusty main" | sudo tee /etc/apt/sources.list.d/ibm-xl-compiler-eval.list
-sudo apt-get update
+#wget -q http://public.dhe.ibm.com/software/server/POWER/Linux/xl-compiler/eval/ppc64le/ubuntu/public.gpg -O- | sudo apt-key add -
+#echo "deb http://public.dhe.ibm.com/software/server/POWER/Linux/xl-compiler/eval/ppc64le/ubuntu/ trusty main" | sudo tee /etc/apt/sources.list.d/ibm-xl-compiler-eval.list
 
-sudo apt-get install xlc.13.1.5
+#sudo apt-get update -qq
+
+wget ftp://public.dhe.ibm.com/software/server/POWER/Linux/rte/xlcpp/le/ubuntu/libxlc*
+
+sudo dpkg -iG *.deb 
+
+ls -l /opt/ibm
+ls -l /opt/ibm/xlC
+ls -l /opt/ibm/xlf
+
 sudo /opt/ibm/xlC/13.1.5/bin/xlc_configure
 
-sudo apt-get install xlf.15.1.5
 sudo /opt/ibm/xlf/15.1.5/bin/xlf_configure
