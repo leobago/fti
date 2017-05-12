8db5b23f7159e77d614e912a65e6305612b4b718
Karol Sierociński <ksiero@man.poznan.pl>
2017-05-12 12:10:51 +0200

    Removed sudo

--- test/tests.sh
+++ test/tests.sh
@@ -5,7 +5,7 @@ startTest () { #$1 - test name $2 - config name; $3 - number of processes; $4 - 
 	echo "		Running $1 test... ($2)"
 	printf "_______________________________________________________________________________________\n\n"
 	cp configs/$2 config.fti
-	sudo mpirun -n $3 ./$1 config.fti 1 $4
+	mpirun -n $3 ./$1 config.fti 1 $4
 	if [ $? != 0 ]
 	then
 		exit 1
@@ -13,7 +13,7 @@ startTest () { #$1 - test name $2 - config name; $3 - number of processes; $4 - 
 	printf "_______________________________________________________________________________________\n\n"	
 	echo "		 Resuming $1 test... ($2)"
 	printf "_______________________________________________________________________________________\n\n"
-	sudo mpirun -n $3 ./$1 config.fti 0 $4
+	mpirun -n $3 ./$1 config.fti 0 $4
 	if [ $? != 0 ]
 	then
 		exit 1
