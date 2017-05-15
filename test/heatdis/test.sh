91e1289a4ee803a22567542aba95ff23388844ce
Karol Sierociński <ksiero@man.poznan.pl>
2017-05-15 08:51:41 +0200

    Trying with sudo
    Former-commit-id: 90c80cd763b8ba549ef0e1570e046b406ffcbba4

--- test/heatdis/test.sh
+++ test/heatdis/test.sh
@@ -2,13 +2,13 @@
 
 test () {
     cp ../configs/$1 ./config.fti
-    mpirun -n $2 ./heatdis config.fti 1
+    sudo mpirun -n $2 ./heatdis config.fti 1
     if [ $? != 0 ]
     then
         exit 1
     fi
     echo "Resuming..."
-    mpirun -n $2 ./heatdis config.fti 0
+    sudo mpirun -n $2 ./heatdis config.fti 0
     if [ $? != 0 ]
     then
         exit 1
