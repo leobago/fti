4b5d242d9d2cf42cddd18c58e87882873f33f9e0
Karol Sierociński <ksiero@man.poznan.pl>
2017-05-22 10:05:00 +0200

    Undo changes

---
+++ Makefile
@@ -0,0 +1,124 @@
+## * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+##* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+##=======================================================================
+##Copyright (C) 2010-2014 Leonardo A. BAUTISTA GOMEZ
+##This program is free software; you can redistribute it and/or modify
+##it under the terms of the GNU General Public License (GPL) as published
+##of the License, or (at your option) any later version.
+##
+##This program is distributed in the hope that it will be useful,
+##but WITHOUT ANY WARRANTY; without even the implied warranty of
+##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##GNU General Public License for more details.
+##
+##To read the license please visit http://www.gnu.org/copyleft/gpl.html
+##=======================================================================
+
+##=======================================================================
+##   PLEASE SET THESE VARIABLES BEFORE COMPILING
+##=======================================================================
+
+FTIPATH		?= /path/to/fti/install/directory/FTI
+BUILDPATH	?= .
+
+##=======================================================================
+##   DIRECTORY TREE
+##=======================================================================
+
+LIB 		= $(BUILDPATH)/lib
+OBJ		= $(BUILDPATH)/obj
+SRC		= src
+DOC		= doc
+INC		= include
+
+##=======================================================================
+##   COMPILERS
+##=======================================================================
+
+CC 		?= cc
+F90 		?= f95
+MPICC 		?= mpicc
+MPIF90		?= mpif90
+# either Gnu, Intel, PGI or XL for now
+BPP_COMPILER_ID ?= Gnu
+
+##=======================================================================
+##   FLAGS
+##=======================================================================
+
+FTIFLAGS	= -fPIC -g -Iinclude/
+
+##=======================================================================
+##   TARGETS
+##=======================================================================
+include vendor/bpp/scripts/bpp.mk
+
+OBJS		= $(OBJ)/galois.o $(OBJ)/jerasure.o \
+		  $(OBJ)/dictionary.o $(OBJ)/iniparser.o \
+		  $(OBJ)/checkpoint.o $(OBJ)/postckpt.o\
+		  $(OBJ)/recover.o $(OBJ)/postreco.o\
+		  $(OBJ)/topo.o $(OBJ)/conf.o $(OBJ)/meta.o \
+		  $(OBJ)/tools.o $(OBJ)/api.o
+
+.PRECIOUS: $(OBJ)/interface.F90
+
+OBJS_F90	= $(OBJ)/interface.o $(OBJ)/ftif.o
+
+SHARED		= libfti.so
+STATIC		= libfti.a
+SHARED_F90	= libfti_f90.so
+STATIC_F90	= libfti_f90.a
+
+all: $(LIB)/$(SHARED) $(LIB)/$(STATIC) $(LIB)/$(SHARED_F90) $(LIB)/$(STATIC_F90)
+
+doc:
+		@mkdir -p $(DOC)
+		doxygen $(DOC)/Doxyfile
+
+$(OBJ)/%.o: $(SRC)/%.c
+		@mkdir -p $(OBJ)
+		$(MPICC) $(FTIFLAGS) -c $< -o $@
+
+$(OBJ)/%: $(SRC)/%.bpp
+		@mkdir -p $(OBJ)
+		$(BPP) $(BPP_DEFAULT_INCLUDES) -DBPP_CONFIG=config.$(BPP_COMPILER_ID) $(BPPOPTS) $< $@
+
+$(OBJ)/%.o: $(OBJ)/%.F90
+		@mkdir -p $(OBJ)
+		@mkdir -p $(LIB)
+		$(MPIF90) $(FTIFLAGS) -c $< -o $@
+		mv *.mod $(INC)/
+
+$(LIB)/$(SHARED): $(OBJS)
+		@mkdir -p $(LIB)
+		$(CC) -shared -o $@ $(OBJS) -lc
+
+$(LIB)/$(SHARED_F90): $(OBJS_F90) $(LIB)/$(SHARED)
+		@mkdir -p $(LIB)
+		$(F90) -shared -o $@ -L$(LIB) -lfti $(OBJS_F90)
+
+$(LIB)/$(STATIC): $(OBJS)
+		@mkdir -p $(LIB)
+		$(RM) $@
+		$(AR) -cvq $@ $(OBJS)
+
+$(LIB)/$(STATIC_F90): $(OBJS_F90)
+		@mkdir -p $(LIB)
+		$(RM) $@
+		$(AR) -cvq $@ $(OBJS_F90)
+
+install: $(LIB)/$(SHARED) $(LIB)/$(STATIC) $(LIB)/$(SHARED_F90) $(LIB)/$(STATIC_F90)
+		install -d $(FTIPATH)/lib  $(FTIPATH)/include
+		install $(INC)/* $(FTIPATH)/include/
+		install $(LIB)/* $(FTIPATH)/lib/
+
+uninstall:
+		$(RM) $(FTIPATH)/$(LIB)/* $(FTIPATH)/$(INC)/*
+		if [ -d "$(FTIPATH)/$(LIB)" ]; then rmdir $(FTIPATH)/$(LIB); fi
+		if [ -d "$(FTIPATH)/$(INC)" ]; then rmdir $(FTIPATH)/$(INC); fi
+		if [ -d "$(FTIPATH)" ]; then rmdir $(FTIPATH); fi
+
+clean:
+		$(RM) $(OBJ)/* $(LIB)/* $(INC)/fti.mod
+
+.PHONY:		doc install uninstall clean
---
+++ examples/heatd2.c
@@ -0,0 +1,186 @@
+/**
+ *  @file   heatdis-fti.c
+ *  @author Leonardo A. Bautista Gomez and Sheng Di
+ *  @date   January, 2014
+ *  @brief  Heat distribution code to test FTI.
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <fti.h>
+
+
+#define ITER_TIMES  5000
+#define ITER_OUT    500
+#define PRECISION   0.001
+#define WORKTAG     26
+#define GRIDSIZE    4096
+
+
+void initData(int nbLines, int M, int rank, double *h)
+{
+    int i, j;
+    for (i = 0; i < nbLines; i++) {
+        for (j = 0; j < M; j++) {
+            h[(i*M)+j] = 0;
+        }
+    }
+    if (rank == 0) {
+        for (j = (M*0.1); j < (M*0.9); j++) {
+            h[j] = 100;
+        }
+    }
+}
+
+
+void print_solution (char *filename, double *grid)
+{
+    int i, j;
+    FILE *outfile;
+    outfile = fopen(filename,"w");
+    if (outfile == NULL) {
+        printf("Can't open output file.");
+        exit(-1);
+    }
+    for (i = 0; i < GRIDSIZE; i++) {
+        for (j = 0; j < GRIDSIZE; j++) {
+            fprintf (outfile, "%6.2f\n", grid[(i*GRIDSIZE)+j]);
+        }
+        fprintf(outfile, "\n");
+    }
+    fclose(outfile);
+}
+
+
+double doWork(int numprocs, int rank, int M, int nbLines, double *g, double *h)
+{
+    int i,j;
+    MPI_Request req1[2], req2[2];
+    MPI_Status status1[2], status2[2];
+    double localerror, globalerror;
+    localerror = 0;
+    for(i = 0; i < nbLines; i++) {
+        for(j = 0; j < M; j++) {
+            h[(i*M)+j] = g[(i*M)+j];
+        }
+    }
+    if (rank > 0) {
+        MPI_Isend(g+M, M, MPI_DOUBLE, rank-1, WORKTAG, FTI_COMM_WORLD, &req1[0]);
+        MPI_Irecv(h,   M, MPI_DOUBLE, rank-1, WORKTAG, FTI_COMM_WORLD, &req1[1]);
+    }
+    if (rank < numprocs-1) {
+        MPI_Isend(g+((nbLines-2)*M), M, MPI_DOUBLE, rank+1, WORKTAG, FTI_COMM_WORLD, &req2[0]);
+        MPI_Irecv(h+((nbLines-1)*M), M, MPI_DOUBLE, rank+1, WORKTAG, FTI_COMM_WORLD, &req2[1]);
+    }
+    if (rank > 0) {
+        MPI_Waitall(2,req1,status1);
+    }
+    if (rank < numprocs-1) {
+        MPI_Waitall(2,req2,status2);
+    }
+    for (i = 1; i < (nbLines-1); i++) {
+        for (j = 0; j < M; j++) {
+            g[(i*M)+j] = 0.25*(h[((i-1)*M)+j]+h[((i+1)*M)+j]+h[(i*M)+j-1]+h[(i*M)+j+1]);
+            if (localerror < fabs(g[(i*M)+j] - h[(i*M)+j])) {
+                localerror = fabs(g[(i*M)+j] - h[(i*M)+j]);
+            }
+        }
+    }
+    if (rank == (numprocs-1)) {
+        for(j = 0; j < M; j++) {
+            g[((nbLines-1)*M)+j] = g[((nbLines-2)*M)+j];
+        }
+    }
+    MPI_Allreduce(&localerror, &globalerror, 1, MPI_DOUBLE, MPI_MAX, FTI_COMM_WORLD);
+    return globalerror;
+}
+
+
+int main(int argc, char *argv[])
+{
+    int rank, nbProcs, nbLines, i, j, N, M, res;
+    double wtime, *h, *g, *grid, globalerror = 1;
+    char fn[32];
+
+    MPI_Init(&argc, &argv);
+    FTI_Init(argv[1], MPI_COMM_WORLD);
+    MPI_Comm_size(FTI_COMM_WORLD, &nbProcs);
+    MPI_Comm_rank(FTI_COMM_WORLD, &rank);
+
+    M = GRIDSIZE;
+    N = GRIDSIZE;
+    nbLines = (N / nbProcs)+3;
+    h = (double *) malloc(sizeof(double *) * M * nbLines);
+    g = (double *) malloc(sizeof(double *) * M * nbLines);
+    grid = (double *) malloc(sizeof(double *) * M * (nbLines-2) * nbProcs);
+    initData(nbLines, M, rank, g);
+    if (rank == 0) {
+        printf("Data initialized. Global grid size is %d x %d.\n", M, (nbLines-2)*nbProcs);
+    }
+
+    // Create your data structure
+    typedef struct cInfo {
+        int id;
+        int level;
+    } cInfo;
+    // Define and initialize the datastructure
+    cInfo myCkpt = {1,1};
+    // Create a new FTI data type
+    FTIT_type ckptInfo;
+    // Initialize the new FTI data type
+    FTI_InitType(&ckptInfo, 2*sizeof(int));
+
+    FTI_Protect(0, &i, 1, FTI_INTG);
+    FTI_Protect(1, &myCkpt, 1, ckptInfo);
+    FTI_Protect(2, h, M*nbLines, FTI_DBLE);
+    FTI_Protect(3, g, M*nbLines, FTI_DBLE);
+
+    MPI_Barrier(FTI_COMM_WORLD);
+    wtime = MPI_Wtime();
+    for(i = 0; i < ITER_TIMES; i++) { // Check execution status
+        if (FTI_Status() != 0) {
+            res = FTI_Recover();
+            if (res != 0) {
+                exit(1);
+            }
+            else { // Update ckpt. id & level
+                myCkpt.level = (myCkpt.level+1)%5; myCkpt.id++;
+            }
+        }
+        else {
+            if (((i+1)%ITER_OUT) == 0) { // Checkpoint every ITER_OUT steps
+                res = FTI_Checkpoint(myCkpt.id, myCkpt.level); // Ckpt ID 5 is ignored because level = 0
+                if (res == 0) {
+                    myCkpt.level = (myCkpt.level+1)%5; myCkpt.id++;
+                } // Update ckpt. id & level
+            }
+        }
+        globalerror = doWork(nbProcs, rank, M, nbLines, g, h);
+        if ((i%ITER_OUT) == 0) {
+            if (rank == 0) {
+                printf("Step : %d, current error = %f; target = %f\n", i, globalerror, PRECISION);
+            }
+            MPI_Gather(g+M, (nbLines-2)*M, MPI_DOUBLE, grid, (nbLines-2)*M, MPI_DOUBLE, 0, FTI_COMM_WORLD);
+            sprintf(fn, "results/vis-%d.dat", i);
+            if (rank == 0) {
+                print_solution(fn, grid);
+            }
+        }
+        if (globalerror < PRECISION) {
+            break;
+        }
+    }
+    if (rank == 0) {
+        printf("Execution finished in %lf seconds.\n", MPI_Wtime() - wtime);
+    }
+
+    free(h);
+    free(g);
+    free(grid);
+
+    FTI_Finalize();
+    MPI_Finalize();
+    return 0;
+}
