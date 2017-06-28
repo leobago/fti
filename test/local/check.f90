8e9d9483903e92b87c788afb91bbaaba357a9db0
Kai Keller <kai.keller@bsc.es>
2017-06-28 15:45:18 +0200

    added fortran unitary test
    Former-commit-id: 59f4537aaf9ffd3b68936cf32cf24ae36d7a196a

--- test/local/check.f90
+++ test/local/check.f90
@@ -1,33 +1,236 @@
+MODULE CHECK_FUNCTIONS
+    IMPLICIT NONE
+    PRIVATE
+    
+    PUBLIC DP, INIT_ARRAYS, VALIDIFY, WRITE_DATA, READ_DATA, &
+        BUFFER, IERROR
+
+    INTEGER, PARAMETER      :: DP = KIND(0.D0)
+    INTEGER, PARAMETER      :: BUFFER = 256
+
+    INTEGER                 :: IERROR
+
+    CONTAINS
+
+        SUBROUTINE INIT_ARRAYS ( A, B )
+            REAL (DP), INTENT(OUT) :: A(:), B(:)
+            A = 1.0_DP
+            CALL RANDOM_NUMBER(B)
+        END SUBROUTINE 
+        
+        SUBROUTINE VALIDIFY ( A, B_CHK, ASIZE, RES )
+            REAL (DP), INTENT(IN)       :: A(:), B_CHK(:)
+            INTEGER (DP), INTENT(IN)    :: ASIZE
+            INTEGER, INTENT(OUT)        :: RES
+            INTEGER                     :: I = 1
+
+            DO
+                IF ( A(I) /= B_CHK(I) ) THEN
+                    RES = -1
+                    EXIT
+                END IF
+                IF ( I == ASIZE ) EXIT
+                I = I+1
+            END DO
+        END SUBROUTINE
+        
+        SUBROUTINE WRITE_DATA (B, ASIZE, RANK)
+            INTEGER, INTENT(IN)         :: RANK
+            INTEGER (DP), INTENT(IN)    :: ASIZE
+            REAL (DP), INTENT(IN)       :: B(:)
+            CHARACTER (LEN=BUFFER)      :: STR
+            INTEGER                     :: RECLEN1, RECLEN2, RECLEN, I
+            
+            WRITE ( STR, '(a,I5.5,a)') 'chk/check-', RANK, ".tst"
+
+            INQUIRE ( IOLENGTH=RECLEN1 ) ( B(I), I=1,ASIZE )
+            INQUIRE ( IOLENGTH=RECLEN2 ) ASIZE
+            RECLEN = RECLEN1 + RECLEN2
+
+            OPEN ( 55, FILE=STR, STATUS='REPLACE', IOSTAT=IERROR, FORM='UNFORMATTED', &
+                ACCESS='DIRECT', RECL=RECLEN, ACTION='WRITE' )
+            
+            WRITE ( 55, REC = 1 ) ASIZE
+            WRITE ( 55, REC = 2 ) B
+
+            CLOSE ( 55 )
+        END SUBROUTINE
+        
+        SUBROUTINE READ_DATA ( B_CHK, ASIZE_CHK, RANK, ASIZE, RES )
+            INTEGER, INTENT(IN)         :: RANK
+            INTEGER (DP), INTENT(IN)    :: ASIZE
+            INTEGER (DP)                :: ASIZE_CHK
+            REAL (DP), INTENT(OUT)      :: B_CHK(:)
+            CHARACTER (LEN=BUFFER)      :: STR
+            INTEGER                     :: RECLEN1, RECLEN2, RECLEN, I
+            INTEGER, INTENT(OUT)        :: RES
+            
+            WRITE ( STR, '(a,I5.5,a)') 'chk/check-', RANK, ".tst"
+            
+            INQUIRE ( IOLENGTH=RECLEN1 ) ASIZE_CHK
+            INQUIRE ( IOLENGTH=RECLEN2 ) ( B_CHK(I), I=1,ASIZE )
+            RECLEN = RECLEN1+RECLEN2
+
+            OPEN ( 55, FILE=STR, STATUS='UNKNOWN', IOSTAT=IERROR, FORM='UNFORMATTED', &
+                ACCESS='DIRECT', RECL=RECLEN, ACTION='READ' )
+            
+            READ ( 55, REC = 1 ) ASIZE_CHK
+            
+            IF ( ASIZE_CHK /= ASIZE ) THEN
+                WRITE(*,'(A, I5.5, A)') "[ERROR -", RANK, "] : wrong dimension"
+                RES = -1
+                CLOSE ( 55 )
+            ELSE 
+                READ ( 55, REC = 2 ) B_CHK
+                CLOSE ( 55 )
+            END IF
+        END SUBROUTINE
+
+END MODULE
+
 PROGRAM CHECK
-      USE MPI
-      USE FTI
-      IMPLICIT NONE
+    USE MPI
+    USE FTI
+    USE CHECK_FUNCTIONS
 
-!**** VARIABLE DEFINITIONS
-      INTEGER                 :: IERROR
-      INTEGER, TARGET         :: FTI_COMM_WORLD
-      INTEGER, PARAMETER      :: BUFFER = 256
-      INTEGER                 :: CRASH, LEVEL, DIFF_SIZES
-      CHARACTER(LEN=BUFFER)   :: CF
-      CHARACTER(LEN=1)   :: CRASH_CHAR, LEVEL_CHAR, DIFF_SIZES_CHAR
+    IMPLICIT NONE
 
-!**** GET ARGUMENTS FROM COMMAND LINE 
-      CALL GETARG(1, CF)
-      CALL GETARG(2, CRASH_CHAR)
-      CALL GETARG(3, LEVEL_CHAR)
-      CALL GETARG(4, DIFF_SIZES_CHAR)
-      READ (CRASH_CHAR(1:1),'(I4)') CRASH
-      READ (LEVEL_CHAR(1:1),'(I4)') LEVEL
-      READ (DIFF_SIZES_CHAR(1:1),'(I4)') DIFF_SIZES
+    !**** CONSTANTS
+    INTEGER, PARAMETER      :: N = 100000
+    INTEGER, PARAMETER      :: CNTRLD_EXIT = 10
+    INTEGER, PARAMETER      :: RECOVERY_FAILED = 20
+    INTEGER, PARAMETER      :: DATA_CORRUPT = 30
+    INTEGER, PARAMETER      :: KEEP = 2
+    INTEGER, PARAMETER      :: RESTART = 1
+    INTEGER, PARAMETER      :: INIT = 0
 
-!**** MAIN PROGRAM STARTS HERE
-      CALL MPI_INIT(IERROR)
-      FTI_COMM_WORLD = MPI_COMM_WORLD
-      CALL FTI_INIT(CF, FTI_COMM_WORLD, IERROR)
+    !**** VARIABLE DEFINITIONS
+    INTEGER                 :: CRASH, LEVEL, DIFF_SIZES
+    INTEGER                 :: RES, TMP, PAR, STATE, SCES = 1
+    INTEGER                 :: FTI_APP_RANK, INVALID
+    INTEGER (DP)            :: ASIZE_CHK
+    INTEGER, TARGET         :: FTI_COMM_WORLD
+    CHARACTER (BUFFER)      :: CF
+    CHARACTER (1)           :: CRASH_CHAR, LEVEL_CHAR, DIFF_SIZES_CHAR
+    
+    !**** DYNAMIC ARRAYS
+    REAL (DP), DIMENSION(:), ALLOCATABLE            :: B_CHK
 
-      PRINT *, "HELLO WORLD!", CF, CRASH, LEVEL, DIFF_SIZES
+    
+    !**** FTI PROTECTED VARIABLES
+    REAL (DP), TARGET, DIMENSION(:), ALLOCATABLE    :: A, B 
+    INTEGER (DP), TARGET    :: ASIZE
 
-      CALL FTI_FINALIZE(IERROR)
-      CALL MPI_FINALIZE(IERROR)
+    !**** POINTER FOR FTI PROTECTED VARS
+    REAL (DP), POINTER      :: A_PTR(:), B_PTR(:) 
+    INTEGER (DP), POINTER   :: ASIZE_PTR
+
+    !**** GET ARGUMENTS FROM COMMAND LINE 
+    CALL GETARG(1, CF)
+    CALL GETARG(2, CRASH_CHAR)
+    CALL GETARG(3, LEVEL_CHAR)
+    CALL GETARG(4, DIFF_SIZES_CHAR)
+    READ (CRASH_CHAR(1:1),'(I4)') CRASH
+    READ (LEVEL_CHAR(1:1),'(I4)') LEVEL
+    READ (DIFF_SIZES_CHAR(1:1),'(I4)') DIFF_SIZES
+
+    !**** MAIN PROGRAM STARTS HERE
+    CALL MPI_INIT(IERROR)
+    FTI_COMM_WORLD = MPI_COMM_WORLD
+    CALL FTI_INIT(CF, FTI_COMM_WORLD, IERROR)
+
+    CALL MPI_COMM_RANK(FTI_COMM_WORLD, FTI_APP_RANK, IERROR)
+    
+    IF ( DIFF_SIZES == 1 ) THEN
+        
+        PAR = MOD( FTI_APP_RANK, 7 )
+        
+        SELECT CASE (PAR) 
+
+            CASE ( 0 )
+                ASIZE = N;
+            CASE ( 1 )
+                ASIZE = 2*N;
+            CASE ( 2 )
+                ASIZE = 3*N;
+            CASE ( 3 )
+                ASIZE = 4*N;
+            CASE ( 4 )
+                ASIZE = 5*N;
+            CASE ( 5 )
+                ASIZE = 6*N;
+            CASE ( 6 )
+                ASIZE = 7*N;
+    
+        END SELECT
+    
+    END IF
+
+    ALLOCATE( A(ASIZE), STAT=IERROR )
+    ALLOCATE( B(ASIZE), STAT=IERROR )
+
+    A_PTR => A
+    B_PTR => B
+    ASIZE_PTR => ASIZE
+
+    CALL FTI_STATUS( STATE );
+
+    CALL FTI_PROTECT(0, A_PTR, IERROR);
+    CALL FTI_PROTECT(1, B_PTR, IERROR);
+    CALL FTI_PROTECT(2, ASIZE_PTR, IERROR);
+
+    IF (STATE == INIT) THEN
+        CALL INIT_ARRAYS ( A, B )
+        CALL WRITE_DATA(B, ASIZE, FTI_APP_RANK);
+        CALL MPI_BARRIER( FTI_COMM_WORLD, IERROR )
+        CALL FTI_Checkpoint( 1, LEVEL, IERROR )
+        CALL SLEEP(2)
+        IF ( CRASH == 1 .AND. FTI_APP_RANK == 0 ) THEN 
+            CALL EXIT(CNTRLD_EXIT)
+        END IF
+    END IF
+
+    IF ( STATE == RESTART .OR. STATE == KEEP ) THEN
+        CALL FTI_Recover( IERROR )
+        IF (IERROR /= 0) THEN
+            CALL EXIT ( RECOVERY_FAILED )
+        END IF
+        ALLOCATE ( B_CHK(ASIZE), STAT=IERROR )
+        CALL READ_DATA ( B_CHK, ASIZE_CHK, FTI_APP_RANK, ASIZE, IERROR )
+        CALL MPI_BARRIER ( FTI_COMM_WORLD, IERROR )
+        IF ( IERROR /= 0 ) THEN
+            CALL EXIT (DATA_CORRUPT)
+        END IF
+    END IF
+
+    !**** CALL VECMULT
+
+    A = A*B
+
+    IF (STATE == RESTART .OR. STATE == KEEP) THEN
+        CALL VALIDIFY ( A, B_CHK, ASIZE, IERROR )
+        CALL MPI_Allreduce( INVALID, TMP, 1, MPI_INT, MPI_SUM, FTI_COMM_WORLD, IERROR);
+        INVALID = TMP;
+    END IF    
+
+    IF ( FTI_APP_RANK == 0 .AND. (STATE == RESTART .OR. STATE == KEEP) ) THEN
+        IF ( INVALID == 0 ) THEN
+            PRINT *, "[SUCCESSFULL]"
+        ELSE
+            PRINT *, "[NOT SUCCESSFULL]"
+            SCES = 0
+        END IF
+    END IF
+    
+    CALL MPI_BARRIER ( FTI_COMM_WORLD, IERROR );
+
+    CALL FTI_FINALIZE(IERROR)
+    CALL MPI_FINALIZE(IERROR)
+    
+    IF ( SCES == 1 ) THEN 
+        CALL EXIT ( 0 )
+    ELSE
+        CALL EXIT ( DATA_CORRUPT )
+    END IF
 END PROGRAM
 
