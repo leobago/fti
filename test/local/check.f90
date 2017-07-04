089e0efea21d9c4bebd4f616d0ccbd211277af4a
Karol Sierociński <ksiero@man.poznan.pl>
2017-07-04 12:43:29 +0200

    Added dots

--- test/local/check.f90
+++ test/local/check.f90
@@ -1,18 +1,18 @@
 !> @author Kai Keller (kellekai@gmx.de)
 !> @date   June, 2017
 !> @brief  Unitary test program for FTI Fortran
-!! 
+!!
 !! The purpose of this code is to test the checkpoint
 !! and restart mechanism in FTI for any case.
 !!
 !! [ USAGE ]
-!! 
+!!
 !! ./PROGRAM ARG1=config ARG2=crash ARG3=level ARG4=diff
 !!
-!> @param ARG1 relative/absolute path to FTI config file 
-!> @param ARG2 1/0 (on/off) - simulate failure  
-!> @param ARG3 checkpoint level (1 - 4) 
-!> @param ARG4 1/0 (on/off) - enable/disable different ckpt sizes  
+!> @param ARG1 relative/absolute path to FTI config file
+!> @param ARG2 1/0 (on/off) - simulate failure
+!> @param ARG3 checkpoint level (1 - 4)
+!> @param ARG4 1/0 (on/off) - enable/disable different ckpt sizes
 
 MODULE CHECK_FUNCTIONS
    IMPLICIT NONE
@@ -25,32 +25,32 @@ MODULE CHECK_FUNCTIONS
    INTEGER                 :: IERROR
 
    CONTAINS
-   
+
    SUBROUTINE INIT_ARRAYS ( A, B )
       REAL, INTENT(OUT)    :: A(:), B(:)
       INTEGER              :: VALUES(1:8), K, I=1
       INTEGER, DIMENSION(:), ALLOCATABLE :: SEED
-   
+
       CALL DATE_AND_TIME(VALUES=VALUES)
       CALL RANDOM_SEED(SIZE=K)
       ALLOCATE(SEED(1:K))
       SEED(:) = VALUES(8)
       CALL RANDOM_SEED(PUT=SEED)
-   
+
       A = 1.0
-   
+
       DO WHILE (I <= SIZE(B))
       CALL RANDOM_NUMBER(B(I))
       I = I+1
       END DO
-   END SUBROUTINE 
-   
+   END SUBROUTINE
+
    SUBROUTINE VALIDIFY ( A, B_CHK, ASIZE, RES )
       REAL, INTENT(IN)           :: A(:), B_CHK(:)
       INTEGER (8), INTENT(IN)    :: ASIZE
       INTEGER, INTENT(OUT)       :: RES
       INTEGER                    :: I = 1
-   
+
       DO
       IF ( A(I) /= B_CHK(I) ) THEN
          RES = -1
@@ -60,29 +60,29 @@ MODULE CHECK_FUNCTIONS
       I = I+1
       END DO
    END SUBROUTINE
-   
+
    SUBROUTINE WRITE_DATA (B, ASIZE, RANK)
       INTEGER, INTENT(IN)           :: RANK
       INTEGER (8), INTENT(IN)       :: ASIZE
       REAL, INTENT(IN)              :: B(:)
       CHARACTER (LEN=BUFFER)        :: STR
       INTEGER                       :: RECLEN1, RECLEN2, RECLEN, I
-   
+
       WRITE ( STR, '(a,I5.5,a)') 'chk/check-', RANK, ".tst"
-   
+
       INQUIRE ( IOLENGTH=RECLEN1 ) ( B(I), I=1,ASIZE )
       INQUIRE ( IOLENGTH=RECLEN2 ) ASIZE
       RECLEN = RECLEN1 + RECLEN2
-   
+
       OPEN ( 55, FILE=STR, STATUS='REPLACE', IOSTAT=IERROR, FORM='UNFORMATTED', &
          ACCESS='DIRECT', RECL=RECLEN, ACTION='WRITE' )
-   
+
       WRITE ( 55, REC = 1 ) ASIZE
       WRITE ( 55, REC = 2 ) B
-   
+
       CLOSE ( 55 )
    END SUBROUTINE
-   
+
    SUBROUTINE READ_DATA ( B_CHK, ASIZE_CHK, RANK, ASIZE, RES )
       INTEGER, INTENT(IN)         :: RANK
       INTEGER (8), INTENT(IN)     :: ASIZE
@@ -91,23 +91,23 @@ MODULE CHECK_FUNCTIONS
       CHARACTER (LEN=BUFFER)      :: STR
       INTEGER                     :: RECLEN1, RECLEN2, RECLEN, I
       INTEGER, INTENT(OUT)        :: RES
-   
+
       WRITE ( STR, '(a,I5.5,a)') 'chk/check-', RANK, ".tst"
-   
+
       INQUIRE ( IOLENGTH=RECLEN1 ) ASIZE_CHK
       INQUIRE ( IOLENGTH=RECLEN2 ) ( B_CHK(I), I=1,ASIZE )
       RECLEN = RECLEN1+RECLEN2
-   
+
       OPEN ( 55, FILE=STR, STATUS='UNKNOWN', IOSTAT=IERROR, FORM='UNFORMATTED', &
          ACCESS='DIRECT', RECL=RECLEN, ACTION='READ' )
-   
+
       READ ( 55, REC = 1 ) ASIZE_CHK
-   
+
       IF ( ASIZE_CHK /= ASIZE ) THEN
          WRITE(*,'(A, I5.5, A)') "[ERROR -", RANK, "] : wrong dimension"
          RES = -1
          CLOSE ( 55 )
-      ELSE 
+      ELSE
          READ ( 55, REC = 2 ) B_CHK
          CLOSE ( 55 )
       END IF
@@ -145,14 +145,14 @@ PROGRAM CHECK
 
 
    !**** FTI PROTECTED VARIABLES
-   REAL, TARGET, DIMENSION(:), ALLOCATABLE    :: A, B 
+   REAL, TARGET, DIMENSION(:), ALLOCATABLE    :: A, B
    INTEGER (8), TARGET     :: ASIZE
 
    !**** POINTER FOR FTI PROTECTED VARS
-   REAL, POINTER           :: A_PTR(:), B_PTR(:) 
+   REAL, POINTER           :: A_PTR(:), B_PTR(:)
    INTEGER (8), POINTER    :: ASIZE_PTR
 
-   !**** GET ARGUMENTS FROM COMMAND LINE 
+   !**** GET ARGUMENTS FROM COMMAND LINE
    CALL GETARG(1, CF)
    CALL GETARG(2, CRASH_CHAR)
    CALL GETARG(3, LEVEL_CHAR)
@@ -168,14 +168,14 @@ PROGRAM CHECK
 
    CALL MPI_COMM_RANK(FTI_COMM_WORLD, FTI_APP_RANK, IERROR)
 
-   ASIZE = N 
+   ASIZE = N
 
    IF ( DIFF_SIZES == 1 ) THEN
 
       PAR = MOD( FTI_APP_RANK, 7 )
-      
-      SELECT CASE (PAR) 
-      
+
+      SELECT CASE (PAR)
+
          CASE ( 0 )
             ASIZE = N;
          CASE ( 1 )
@@ -190,7 +190,7 @@ PROGRAM CHECK
             ASIZE = 6*N;
          CASE ( 6 )
             ASIZE = 7*N;
-      
+
       END SELECT
 
    END IF
@@ -214,7 +214,7 @@ PROGRAM CHECK
       CALL MPI_BARRIER ( FTI_COMM_WORLD, IERROR )
       CALL FTI_CHECKPOINT ( 1, LEVEL, IERROR )
       CALL SLEEP(2)
-      IF ( CRASH == 1 .AND. FTI_APP_RANK == 0 ) THEN 
+      IF ( CRASH == 1 .AND. FTI_APP_RANK == 0 ) THEN
          CALL EXIT ( CNTRLD_EXIT )
       END IF
    END IF
@@ -240,13 +240,13 @@ PROGRAM CHECK
       CALL VALIDIFY ( A, B_CHK, ASIZE, INVALID )
       CALL MPI_Allreduce( INVALID, TMP, 1, MPI_INT, MPI_SUM, FTI_COMM_WORLD, IERROR);
       INVALID = TMP;
-   END IF    
+   END IF
 
    IF ( FTI_APP_RANK == 0 .AND. (STATE == RESTART .OR. STATE == KEEP) ) THEN
       IF ( INVALID == 0 ) THEN
-         PRINT *, "[successful]"
+         PRINT *, "[SUCCESSFUL]"
       ELSE
-         PRINT *, "[NOT successful]"
+         PRINT *, "[NOT SUCCESSFUL]"
          SCES = 0
       END IF
    END IF
@@ -256,10 +256,9 @@ PROGRAM CHECK
    CALL FTI_FINALIZE(IERROR)
    CALL MPI_FINALIZE(IERROR)
 
-   IF ( SCES == 1 ) THEN 
+   IF ( SCES == 1 ) THEN
       CALL EXIT ( 0 )
    ELSE
       CALL EXIT ( DATA_CORRUPT )
    END IF
 END PROGRAM
-
