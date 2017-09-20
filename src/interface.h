88d1ffb4b6fd97cc17868cbb0b202aedb2666b65
Kai Keller <kai.keller@bsc.es>
2017-09-20 12:31:25 +0200

    added syncIterMax variable FTIT_execution struct.

--- src/interface.h
+++ src/interface.h
@@ -75,7 +75,7 @@ int FTI_ReadConf(FTIT_configuration* FTI_Conf, FTIT_execution* FTI_Exec,
                  FTIT_topology* FTI_Topo, FTIT_checkpoint* FTI_Ckpt,
                  FTIT_injection *FTI_Inje);
 int FTI_TestConfig(FTIT_configuration* FTI_Conf, FTIT_topology* FTI_Topo,
-                   FTIT_checkpoint* FTI_Ckpt);
+                   FTIT_checkpoint* FTI_Ckpt, FTIT_execution* FTI_Exec);
 int FTI_TestDirectories(FTIT_configuration* FTI_Conf, FTIT_topology* FTI_Topo);
 int FTI_CreateDirs(FTIT_configuration* FTI_Conf, FTIT_execution* FTI_Exec,
                    FTIT_topology* FTI_Topo, FTIT_checkpoint* FTI_Ckpt);
