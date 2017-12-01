6f95c3975705edc0523975771cf0b917d705ebf5
Tomasz <tomaszp@man.poznan.pl>
2017-12-01 15:38:06 +0100

    Added hdf5test

--- test/hdf5Test.c
+++ test/hdf5Test.c
@@ -1,21 +1,20 @@
 /**
- *  @file   addInArray.c
- *  @author Karol Sierocinski (ksiero@man.poznan.pl)
- *  @date   Feburary, 2017
+ *  @file   hdf5Test.c
+ *  @author Tomasz Paluszkiewicz (tomaszp@man.poznan.pl)
+ *  @date   November, 2017
  *  @brief  FTI testing program.
  *
- *  Testing FTI_Init, FTI_Checkpoint, FTI_Status, FTI_Recover, FTI_Finalize,
+ *  Testing FTI_InitType, FTI_InitSimpleTypeWithNames,
+ *  FTI_InitComplexTypeWithNames, FTI_ProtectWithName, FTI_Checkpoint, FTI_Recover,
  *  saving last checkpoint to PFS
  *
- *  Program adds number in array, does MPI_Allgather each iteration and checkpoint
- *  every ITER_CHECK interations with level passed in argv, but recovery is always
- *  from L4, because of FTI_Finalize() call.
+ *  Program creates complex data structures, then adds it to protect list
+ *  and makes a checkpoint. Second run recovers the files and checks if
+ *  values match.
  *
  *  First execution this program should be with fail flag = 1, because
- *  then FTI saves checkpoint and program stops after ITER_STOP iteration.
- *  Second execution must be with the same #defines and flag = 0 to
- *  properly recover data. It is important that FTI config file got
- *  keep_last_ckpt = 1.
+ *  then FTI saves checkpoint and program stops. Second execution
+ *  must be with the flag = 0 to properly recover data.
  */
 
 #include <stdio.h>
@@ -27,6 +26,9 @@
 
 #include "../deps/iniparser/iniparser.h"
 #include "../deps/iniparser/dictionary.h"
+
+#define VERIFY_SUCCESS 0
+#define VERIFY_FAILED 1
 
 typedef struct AsByteArray {
     char character;
@@ -72,132 +74,204 @@ typedef struct AllTypes {
     FloatsChars floatsChars;
 } AllTypes;
 
-void defaultChars(Chars * in) {
+void defaultChars(Chars* in, int shift) {
     sprintf(in->chars, "%s", "abcdefg");
     int i, j, k, l;
+
+    for (i = 0; i < 7; i++)
+        in->chars[i] += shift % (25 - in->chars[i] - 'a');
+
     for (i = 0; i < 2; i++) {
         for (j = 0; j < 3; j++) {
             for (k = 0; k < 4; k++) {
                 for (l = 0; l < 5; l++) {
-                    in->uChars[i][j][k][l] = 'a' + (i + j + k + l) % 25;
+                    in->uChars[i][j][k][l] = 'a' + ((i + j + k + l) + shift) % 25;
                 }
             }
+        }
+        in->bytes[i].character = 'C' + shift % 23;
+        for (j = 0; j < 1024; j++) {
+            in->bytes[i].longs[j] = (j + 1) * 2 + shift;
         }
     }
 }
 
-void defaultInts(Integers * in) {
-    in->shortInteger = -12;
-    in->integer = -123;
-    in->longInteger = -1234;
+void defaultInts(Integers* in, int shift) {
+    in->shortInteger = -12 - shift;
+    in->integer = -123 - shift;
+    in->longInteger = -1234 - shift;
 }
 
-void defaultUInts(UIntegers * in) {
-    in->shortInteger = 12;
-    in->integer = 123;
-    in->longInteger = 1234;
+void defaultUInts(UIntegers* in, int shift) {
+    in->shortInteger = 12 + shift;
+    in->integer = 123 + shift;
+    in->longInteger = 1234 + shift;
 }
 
-void defaultFloats(Floats * in) {
-    in->singlePrec = 12.5f;
-    in->doublePrec = 123.25;
-    in->longDoublePrec = 1234.125;
+void defaultFloats(Floats* in, int shift) {
+    in->singlePrec = 12.5f + shift;
+    in->doublePrec = 123.25 + shift;
+    in->longDoublePrec = 1234.125 + shift;
 }
 
-void defaultAllInts(AllInts * in) {
+void defaultAllInts(AllInts* in, int shift) {
     int i;
     for(i = 0; i < 5; i++) {
-        defaultInts(in->integers + i);
+        defaultInts(in->integers + i, shift + i);
     }
 
     for(i = 0; i < 4; i++) {
-        defaultUInts(in->uIntegers + i);
+        defaultUInts(in->uIntegers + i, shift + i);
     }
 }
 
-void defaultFloatsChars(FloatsChars * in) {
+void defaultFloatsChars(FloatsChars* in, int shift) {
     int i;
     for(i = 0; i < 3; i++) {
-        defaultFloats(in->floats + i);
+        defaultFloats(in->floats + i, shift + i);
     }
 
     for(i = 0; i < 2; i++) {
-        defaultChars(in->chars + i);
+        defaultChars(in->chars + i, shift + i);
     }
 }
 
-void defaultAllTypes(AllTypes * in) {
-    defaultAllInts(&in->allInts);
-    defaultFloatsChars(&in->floatsChars);
+void defaultAllTypes(AllTypes* in, int shift) {
+    defaultAllInts(&in->allInts, shift);
+    defaultFloatsChars(&in->floatsChars, shift);
 }
 
 
-int verifyChars(Chars * in) {
-    if (strcmp(in->chars, "abcdefg"))
-        return 1;
-    int i, j, k, l;
+int verifyChars(Chars* in, int shift, int rank, char* name) {
+    char buff[] = "abcdefg";
+    int i;
+    for (i = 0; i < 7; i++)
+        buff[i] += shift % (25 - buff[i]- 'a');
+    if (strcmp(in->chars, buff)) {
+        printf("[ %06d ] : %s.chars = %s should be %s \n", rank, name, in->chars, buff);
+        return VERIFY_FAILED;
+    }
+    int j, k, l;
     for (i = 0; i < 2; i++) {
         for (j = 0; j < 3; j++) {
             for (k = 0; k < 4; k++) {
                 for (l = 0; l < 5; l++) {
-                    if (in->uChars[i][j][k][l] != 'a' + (i + j + k + l) % 25)
-                        return 1;
+                    if (in->uChars[i][j][k][l] != 'a' + ((i + j + k + l) + shift) % 25) {
+                        printf("[ %06d ] : %s.uChars[%d][%d][%d][%d] = %u should be %u \n", rank, name,
+                                i, j, k, l, in->uChars[i][j][k][l], 'a' + ((i + j + k + l) + shift) % 25);
+                        return VERIFY_FAILED;
+                    }
                 }
             }
         }
+        if (in->bytes[i].character != 'C' + shift % 23) {
+            printf("[ %06d ] : %s.bytes[%d].character = %c should be %c \n",
+                    rank, name, i, in->bytes[i].character, ('C' + shift % 23));
+            return VERIFY_FAILED;
+        }
+        for (j = 0; j < 1024; j++) {
+            if (in->bytes[i].longs[j] != (j + 1) * 2 + shift) {
+                printf("[ %06d ] : %s.bytes[%d].longs[%d] = %ld should be %d \n",
+                        rank, name, i, j, in->bytes[i].longs[j], (j + 1) * 2 + shift);
+                return VERIFY_FAILED;
+            }
+        }
     }
-    return 0;
+    return VERIFY_SUCCESS;
 }
 
-int verifyInts(Integers * in) {
-    if (in->shortInteger != -12 || in->integer != -123 || in->longInteger != -1234)
-        return 1;
-    return 0;
+int verifyInts(Integers* in, int shift, int rank, char* name) {
+    if (in->shortInteger != -12 - shift) {
+        printf("[ %06d ] : %s.shortInteger = %hd should be %hd \n", rank, name, in->shortInteger, -12 - shift);
+        return VERIFY_FAILED;
+    }
+    if (in->integer != -123 - shift) {
+        printf("[ %06d ] : %s.shortInteger = %d should be %d \n", rank, name, in->integer, -123 - shift);
+        return VERIFY_FAILED;
+    }
+    if (in->longInteger != -1234 - shift) {
+        printf("[ %06d ] : %s.shortInteger = %ld should be %d \n", rank, name, in->longInteger, -1234 - shift);
+        return VERIFY_FAILED;
+    }
+    return VERIFY_SUCCESS;
 }
 
-int verifyUInts(UIntegers * in) {
-    if (in->shortInteger != 12 || in->integer != 123 || in->longInteger != 1234)
-        return 1;
-    return 0;
+int verifyUInts(UIntegers* in, int shift, int rank, char* name) {
+    if (in->shortInteger != 12 + shift || in->integer != 123 + shift || in->longInteger != 1234 + shift)
+        return VERIFY_FAILED;
+
+    if (in->shortInteger != 12 + shift) {
+        printf("[ %06d ] : %s.shortInteger = %hu should be %hu \n", rank, name, in->shortInteger, 12 + shift);
+        return VERIFY_FAILED;
+    }
+    if (in->integer != 123 + shift) {
+        printf("[ %06d ] : %s.shortInteger = %u should be %u \n", rank, name, in->integer, 123 + shift);
+        return VERIFY_FAILED;
+    }
+    if (in->longInteger != 1234 + shift) {
+        printf("[ %06d ] : %s.shortInteger = %lu should be %u \n", rank, name, in->longInteger, 1234 + shift);
+        return VERIFY_FAILED;
+    }
+    return VERIFY_SUCCESS;
 }
 
-int verifyFloats(Floats * in) {
-    if (in->singlePrec != 12.5f || in->doublePrec != 123.25 || in->longDoublePrec != 1234.125)
-        return 1;
-    return 0;
+int verifyFloats(Floats * in, int shift, int rank, char* name) {
+    if (in->singlePrec != 12.5f + shift) {
+        printf("[ %06d ] : %s.singlePrec = %f should be %f \n", rank, name, in->singlePrec, 12.5f + shift);
+        return VERIFY_FAILED;
+    }
+    if (in->doublePrec != 123.25 + shift) {
+        printf("[ %06d ] : %s.doublePrec = %f should be %f \n", rank, name, in->doublePrec, 123.25 + shift);
+        return VERIFY_FAILED;
+    }
+    if (in->longDoublePrec != 1234.125 + shift) {
+        printf("[ %06d ] : %s.longDoublePrec = %Lf should be %f \n", rank, name, in->longDoublePrec, 1234.125 + shift);
+        return VERIFY_FAILED;
+    }
+    return VERIFY_SUCCESS;
 }
 
-int verifyAllInts(AllInts * in) {
+int verifyAllInts(AllInts * in, int shift, int rank, char* name) {
     int i, res = 0;
+    char buff[256];
     for(i = 0; i < 5; i++) {
-        res += verifyInts(&(in->integers[i]));
+        sprintf(buff, "%s.integers[%d]", name, i);
+        res += verifyInts(&(in->integers[i]), shift + i, rank, buff);
     }
 
     for(i = 0; i < 4; i++) {
-        res += verifyUInts(&(in->uIntegers[i]));
+        sprintf(buff, "%s.uIntegers[%d]", name, i);
+        res += verifyUInts(&(in->uIntegers[i]), shift + i, rank, buff);
     }
     return res;
 }
 
-int verifyFloatsChars(FloatsChars * in) {
+int verifyFloatsChars(FloatsChars * in, int shift, int rank, char* name) {
     int i, res = 0;
+    char buff[256];
     for(i = 0; i < 3; i++) {
-        res += verifyFloats(&(in->floats[i]));
+        sprintf(buff, "%s.floats[%d]", name, i);
+        res += verifyFloats(&(in->floats[i]), shift + i, rank, buff);
     }
 
     for(i = 0; i < 2; i++) {
-        res += verifyChars(&(in->chars[i]));
+        sprintf(buff, "%s.chars[%d]", name, i);
+        res += verifyChars(&(in->chars[i]), shift + i, rank, buff);
     }
     return res;
 }
 
-int verifyAllTypes(AllTypes * in) {
-    return verifyAllInts(&(in->allInts)) + verifyFloatsChars(&(in->floatsChars));
+int verifyAllTypes(AllTypes * in, int shift, int rank, char* name) {
+    char buff[256];
+    sprintf(buff, "%s.allInts", name);
+    int res = verifyAllInts(&(in->allInts), shift, rank, buff);
+    sprintf(buff, "%s.floatsChars", name);
+    res += verifyFloatsChars(&(in->floatsChars), shift, rank, buff);
+    return res;
 }
 
 
-int init(char** argv, int* checkpoint_level, int* fail)
-{
+int init(char** argv, int* checkpoint_level, int* fail) {
     int rtn = 0;    //return value
     if (argv[1] == NULL) {
         printf("Missing first parameter (config file).\n");
@@ -225,8 +299,7 @@ int init(char** argv, int* checkpoint_level, int* fail)
     @return     integer     0 if successful, 1 otherwise
  **/
 /*-------------------------------------------------------------------------*/
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     int checkpoint_level, fail;
     init(argv, &checkpoint_level, &fail);
     MPI_Init(&argc, &argv);
@@ -331,7 +404,6 @@ int main(int argc, char** argv)
     AllIntsDef.size = sizeof(AllInts);
     FTI_InitComplexTypeWithNames(&AllIntsType, &AllIntsDef);
 
-
     FTIT_complexType FloatsCharsDef;
     FTIT_type FloatsCharsType;
     FloatsCharsDef.field[0].type = &FloatsType;
@@ -364,6 +436,7 @@ int main(int argc, char** argv)
 
     Chars charVars[2];
     Integers intVars[2];
+    Integers intVars2;
     UIntegers uintVars;
     Floats floatVars;
     AllInts allIntVars;
@@ -378,20 +451,20 @@ int main(int argc, char** argv)
     FTI_ProtectWithName(5, &allIntVars, 1, AllIntsType, "all ints");
     FTI_ProtectWithName(6, &floatCharVars, 1, FloatsCharsType, "floats and chars");
     FTI_ProtectWithName(7, &allTypesVar, 2, AllTypesType, "all types");
-    FTI_ProtectWithName(8, &intVars, 1, IntegersType, "ints2");
-
+    FTI_ProtectWithName(8, &intVars2, 1, IntegersType, "ints2");
 
     if (fail == 1) {
-        defaultChars(charVars);
-        defaultChars((charVars + 1));
-        defaultInts(intVars);
-        defaultInts((intVars + 1));
-        defaultUInts(&uintVars);
-        defaultFloats(&floatVars);
-        defaultAllInts(&allIntVars);
-        defaultFloatsChars(&floatCharVars);
-        defaultAllTypes(allTypesVar);
-        defaultAllTypes((allTypesVar + 1));
+        defaultChars(charVars, 0);
+        defaultChars((charVars + 1), 1);
+        defaultInts(intVars, 0);
+        defaultInts((intVars + 1), 1);
+        defaultInts(&intVars2, 5);
+        defaultUInts(&uintVars, 0);
+        defaultFloats(&floatVars, 0);
+        defaultAllInts(&allIntVars, 2);
+        defaultFloatsChars(&floatCharVars, 2);
+        defaultAllTypes(allTypesVar, 3);
+        defaultAllTypes((allTypesVar + 1), 4);
 
         FTI_Checkpoint(2, checkpoint_level);
 
@@ -431,30 +504,31 @@ int main(int argc, char** argv)
         MPI_Barrier(FTI_COMM_WORLD);
         //There is no FTI_Finalize(), because want to recover also from L1, L2, L3
         MPI_Finalize();
-        //FTI_Finalize();
-        //MPI_Finalize();
     }
     else {
         FTI_Recover();
         FTI_Checkpoint(3, checkpoint_level);
         int res = 0;
-        res += verifyChars(charVars);
-        res += verifyChars((charVars + 1));
-        res += verifyInts(intVars);
-        res += verifyInts((intVars + 1));
-        res += verifyUInts(&uintVars);
-        res += verifyFloats(&floatVars);
-        res += verifyAllInts(&allIntVars);
-        res += verifyFloatsChars(&floatCharVars);
-        res += verifyAllTypes(allTypesVar);
-        res += verifyAllTypes((allTypesVar + 1));
+        res += verifyChars(charVars, 0, world_rank, "charVars[0]");
+        res += verifyChars((charVars + 1),  1, world_rank, "charVars[1]");
+        res += verifyInts(intVars, 0, world_rank, "intVars[0]");
+        res += verifyInts((intVars + 1), 1, world_rank, "intVars[1]");
+        res += verifyInts(&intVars2, 5, world_rank, "intVars2");
+        res += verifyUInts(&uintVars,  0, world_rank, "uIntVars");
+        res += verifyFloats(&floatVars, 0, world_rank, "floatVars");
+        res += verifyAllInts(&allIntVars, 2, world_rank, "allIntVars");
+        res += verifyFloatsChars(&floatCharVars, 2, world_rank, "floatCharVars");
+        res += verifyAllTypes(allTypesVar, 3, world_rank, "allTypesVar[0]");
+        res += verifyAllTypes((allTypesVar + 1), 4, world_rank, "allTypesVar[1]");
 
         FTI_Finalize();
         MPI_Finalize();
-        if (res != 0) {
+        if (res != VERIFY_SUCCESS && world_rank == 0) {
             printf("Test failed!\n");
             return 1;
         }
+        if (world_rank == 0)
+            printf("Success.\n");
     }
     return 0;
 }
