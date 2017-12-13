046dc420e069e9f2f77d28f099408ffc8646931e
Karol Sierociński <ksiero@man.poznan.pl>
2017-12-13 09:42:09 +0100

    Using CPack to create package.

--- src/interface.h
+++ src/interface.h
@@ -52,7 +52,20 @@
 #endif
 
 #include <stdint.h>
-#include "../deps/md5/md5.h"
+
+//extern functions for FTI package
+typedef unsigned int MD5_u32plus;
+typedef struct {
+	MD5_u32plus lo, hi;
+	MD5_u32plus a, b, c, d;
+	unsigned char buffer[64];
+	MD5_u32plus block[16];
+} MD5_CTX;
+
+extern void MD5_Init(MD5_CTX *ctx);
+extern void MD5_Update(MD5_CTX *ctx, const void *data, unsigned long size);
+extern void MD5_Final(unsigned char *result, MD5_CTX *ctx);
+
 #define CHUNK_SIZE 131072    /**< MD5 algorithm chunk size.      */
 
 #include <sys/stat.h>
