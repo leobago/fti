1dbb9ed09f40bb232659bcf2c5ef54a99ef89b2b
Kai Keller <kai.keller@bsc.es>
2017-12-21 12:41:07 +0100

    fixes the case keep_last_ckpt = 1 for MPI-IO

--- src/postckpt.c
+++ src/postckpt.c
@@ -487,12 +487,9 @@ int FTI_Flush(FTIT_configuration* FTI_Conf, FTIT_execution* FTI_Exec,
     if (res != FTI_SCES) {
         return FTI_NSCS;
     }
-    //if (!FTI_Ckpt[4].isInline || FTI_Conf->ioMode == FTI_IO_POSIX) {
-    //    //Just copy checkpoint files to PFS if L4 post-processing done by heads
-    //    res = FTI_FlushPosix(FTI_Conf, FTI_Exec, FTI_Topo, FTI_Ckpt, level);
-    //}
-    //else {
+
     switch(FTI_Conf->ioMode) {
+        
         case FTI_IO_POSIX:
             FTI_FlushPosix(FTI_Conf, FTI_Exec, FTI_Topo, FTI_Ckpt, level);
             break;
