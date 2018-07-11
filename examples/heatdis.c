b383508617740f29f30c7f4fcc5bbb37aab9bd30
Kai Keller <kai.keller@bsc.es>
2018-07-11 17:21:41 +0200

    recovered heatdist.c from master

--- examples/heatdis.c
+++ examples/heatdis.c
@@ -9,132 +9,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <stdbool.h>
 #include <fti.h>
-#include <time.h>
 
 
-#define PRECISION   0.0000001
-#define ITER_TIMES  1000000
-#define ITER_OUT    5
+#define PRECISION   0.005
+#define ITER_TIMES  5000
+#define ITER_OUT    500
 #define WORKTAG     50
 #define REDUCE      5
 
-int FIRST_CHECKPOINT;
-int SECOND_CHECKPOINT;
-int CHECKPOINT_ITER;
-int LAST_CHECKPOINT;
-bool isRestart;
-bool toRecover;
-bool toFinalize;
-bool isCheckpointed;
-
-void Snapshot_distance( int iter, double wtime, int rank ) {
-    
-    static double ckpt1_t;
-
-    if ( isRestart ) {
-        if ( toRecover ) {
-            FTI_Recover();
-            toRecover = false;
-        }
-        if ( toFinalize ) {
-            FTI_Finalize();
-            MPI_Finalize();
-            exit(EXIT_SUCCESS);
-        }
-        if ( (iter%5) == 0 ) {
-            toFinalize = true;
-        }
-    } else {
-        if ( iter == FIRST_CHECKPOINT ) {
-            FTI_Checkpoint( iter, 4 );
-            ckpt1_t = MPI_Wtime();
-        }
-        if ( iter == SECOND_CHECKPOINT ) {
-            if (rank == 0) {
-                printf("Second checkpoint after %lf seconds.\n", MPI_Wtime() - ckpt1_t);
-            }
-            FTI_Checkpoint( iter, 4 );
-            isCheckpointed = true;
-        }
-        if ( toFinalize ) {
-            if (rank == 0) {
-                printf("Execution finished in %lf seconds.\n", MPI_Wtime() - wtime);
-            }
-            MPI_Finalize();
-            exit(EXIT_SUCCESS);
-        }
-        if ( (iter%5) == 0 && isCheckpointed ) {
-            toFinalize = true;
-        }
-    }
-}
-
-void Snapshot( int iter, double wtime, int rank, double *ptr1, double *ptr2 ) {
-    
-    static double ckpt1_t;
-
-    if ( isRestart ) {
-        if ( toRecover ) {
-            FTI_Recover();
-            toRecover = false;
-        }
-        if ( toFinalize ) {
-            FTI_Finalize();
-            MPI_Finalize();
-            exit(EXIT_SUCCESS);
-        }
-        if ( (iter%5) == 0 ) {
-            toFinalize = true;
-        }
-    } else {
-        if ( (iter%CHECKPOINT_ITER == 0) && iter > 0 ) {
-            FTI_Checkpoint( iter, 4 );
-            ckpt1_t = MPI_Wtime();
-        }
-        if ( iter == LAST_CHECKPOINT ) {
-            if (rank == 0) {
-                printf("Execution finished in %lf seconds.\n", MPI_Wtime() - wtime);
-            }
-            free(ptr1);
-            free(ptr2);
-            FTI_Finalize();
-            MPI_Finalize();
-            exit(EXIT_SUCCESS);
-        }
-    }
-}
 
 void initData(int nbLines, int M, int rank, double *h)
 {
     int i, j;
-    srand(time(NULL));
     for (i = 0; i < nbLines; i++) {
         for (j = 0; j < M; j++) {
-            if((i%2 == 0) && (rank%4 == 0)) {
-                if(j%2 == 0) {
-                    h[(i*M)+j] = rand()%100+1;
-                } else {
-                    h[(i*M)+j] = 0;
-                }
-            }
-            else if((i%2 != 0) && (rank%4 == 0)) {
-                if(j%2 != 0) {
-                    h[(i*M)+j] = rand()%100+1;
-                } else {
-                    h[(i*M)+j] = 0;
-                }
-            } else {
-                h[(i*M)+j] = 0;
-            }
+            h[(i*M)+j] = 0;
         }
     }
-//    if (rank%2 == 0) {
-//        for (j = (M*0.1); j < (M*0.9); j++) {
-//            h[j] = rand()%100+1;
-//        }
-//    }
+    if (rank == 0) {
+        for (j = (M*0.1); j < (M*0.9); j++) {
+            h[j] = 100;
+        }
+    }
 }
 
 
@@ -188,22 +85,10 @@ int main(int argc, char *argv[])
 
     MPI_Init(&argc, &argv);
     FTI_Init(argv[2], MPI_COMM_WORLD);
-    
+
     MPI_Comm_size(FTI_COMM_WORLD, &nbProcs);
     MPI_Comm_rank(FTI_COMM_WORLD, &rank);
-    
-    toFinalize = false;
-    isCheckpointed = false;
-    if ( FTI_Status() == 1 ) {
-        toRecover = true;
-        isRestart = true;
-    }
 
-    //FIRST_CHECKPOINT = atoi(getenv("FTI_FIRST_CKPT"));
-    //SECOND_CHECKPOINT = atoi(getenv("FTI_SECOND_CKPT"));
-    CHECKPOINT_ITER = atoi(getenv("FTI_CKPT_ITER"));
-    LAST_CHECKPOINT = atoi(getenv("FTI_LAST_CKPT"));
-    
     arg = atoi(argv[1]);
     M = (int)sqrt((double)(arg * 1024.0 * 512.0 * nbProcs)/sizeof(double));
     nbLines = (M / nbProcs)+3;
@@ -224,13 +109,13 @@ int main(int argc, char *argv[])
 
     wtime = MPI_Wtime();
     for (i = 0; i < ITER_TIMES; i++) {
-        Snapshot( i, wtime, rank, g, h );
+        int checkpointed = FTI_Snapshot();
         localerror = doWork(nbProcs, rank, M, nbLines, g, h);
-        if ((i%REDUCE) == 0) {
-            MPI_Allreduce(&localerror, &globalerror, 1, MPI_DOUBLE, MPI_MAX, FTI_COMM_WORLD);
-        }
         if (((i%ITER_OUT) == 0) && (rank == 0)) {
             printf("Step : %d, error = %f\n", i, globalerror);
+        }
+        if ((i%REDUCE) == 0) {
+            MPI_Allreduce(&localerror, &globalerror, 1, MPI_DOUBLE, MPI_MAX, FTI_COMM_WORLD);
         }
         if(globalerror < PRECISION) {
             break;
