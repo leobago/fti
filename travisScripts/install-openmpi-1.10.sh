d932c822de639e811f9d7c6e5e4a551ffbd8d617
Kai Keller <kai.keller@bsc.es>
2018-08-01 15:18:40 +0200

    install openmpi-1.10 on travis

---
+++ travisScripts/install-openmpi-1.10.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget --no-check-certificate https://www.open-mpi.org/software/ompi/v1.10/downloads/openmpi-1.10.7.tar.gz
+tar -zxf openmpi-1.10.7.tar.gz
+cd openmpi-1.10.7
+bash ./configure --prefix=$HOME/openmpi-1.10
+make -j
+sudo make install
+export PATH="$PATH:$HOME/openmpi-1.10/bin"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/openmpi-1.10/lib/"
