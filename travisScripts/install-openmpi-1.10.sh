59575c644f3e87e4953c89b691df97851f029653
Kai Keller <kai.keller@bsc.es>
2018-08-01 16:15:58 +0200

    hide output for openmpi installation

--- travisScripts/install-openmpi-1.10.sh
+++ travisScripts/install-openmpi-1.10.sh
@@ -2,8 +2,8 @@
 wget --no-check-certificate https://www.open-mpi.org/software/ompi/v1.10/downloads/openmpi-1.10.7.tar.gz
 tar -zxf openmpi-1.10.7.tar.gz
 cd openmpi-1.10.7
-bash ./configure --prefix=$HOME/openmpi-1.10
-make -j
-sudo make install
+bash ./configure --prefix=$HOME/openmpi-1.10 > /dev/null
+make -j > /dev/null
+sudo make install > /dev/null
 export PATH=$PATH:$HOME/openmpi-1.10/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/openmpi-1.10/lib/
