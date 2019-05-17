47616df12ac62a3ba4d6a6a51d0d8b44b7cc3e49
Kai Keller <kai.keller@bsc.es>
2019-05-17 12:47:54 +0200

    1) disable fortran per default 2) fixes bug if openssl not found

--- deps/md5/md5.c
+++ deps/md5/md5.c
@@ -304,7 +304,7 @@ unsigned char *MD5(void *pointer, size_t pointerLength , unsigned char *md5HashP
     if ( pointer == NULL || pointerLength == 0 ) {
         MD5_Update( &mdContext, staticArray, MD5_CBLOCK );
     } else {
-        MD5_Update( &mdContext, pointer, pointeeLength );
+        MD5_Update( &mdContext, pointer, pointerLength );
     }
 
     if ( md5HashPointer == NULL ) {
