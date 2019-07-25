dbb69c5838191a34e14b450f253c353a813c2464
Kai Keller <kai.keller@bsc.es>
2019-07-25 11:08:37 +0200

    works for manual testing, no unit test yet

--- src/topo.h
+++ src/topo.h
@@ -5,9 +5,11 @@ int FTI_ReorderNodes(FTIT_configuration* FTI_Conf, FTIT_topology* FTI_Topo,
         int *nodeList, char *nameList);
 int FTI_BuildNodeList(FTIT_configuration* FTI_Conf, FTIT_execution* FTI_Exec,
         FTIT_topology* FTI_Topo, int *nodeList, char *nameList);
+int FTI_CreateGroupTopology( FTIT_configuration* FTI_Conf, FTIT_execution* FTI_Exec, FTIT_topology* FTI_Topo, 
+        int * nodeList, int * group, int * distProcList );
 int FTI_CreateComms(FTIT_configuration* FTI_Conf, FTIT_execution* FTI_Exec,
         FTIT_topology* FTI_Topo, int *userProcList,
-        int *distProcList, int* nodeList);
+        int *distProcList, int* nodeList, int* group);
 int FTI_Topology(FTIT_configuration* FTI_Conf, FTIT_execution* FTI_Exec,
         FTIT_topology* FTI_Topo);
 #endif // __TOPO_H__
