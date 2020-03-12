8e88b096984e344eb7bd96e9d4ab83363f1ef50f
Kai Keller <kai.keller@bsc.es>
2020-03-12 18:58:40 +0100

    added doxygen comments to iniparser.h

--- src/util/dataset.h
+++ src/util/dataset.h
@@ -1,6 +1,25 @@
 #ifndef FTI_DATASET_H
 #define FTI_DATASET_H
 
+/**--------------------------------------------------------------------------
+  
+  
+  @brief        Initializes instance of FTIT_dataset.
+
+  This function initializes the dataset to 0 and all the members with non-zero
+  default values to the respective ones.
+
+  @param        FTI_Exec[in]    <b> FTIT_execution* </b>    FTI execution
+  metadata.
+  @param        data[out]       <b> FTIT_dataset* </b>      Pointer to dataset
+  to be initialized.
+  @param        id[in]          <b> int </b>                id of dataset to
+  be initialized.
+  
+  @return                       \ref FTI_SCES  
+ 
+
+--------------------------------------------------------------------------**/
 int FTI_InitDataset( FTIT_execution* FTI_Exec, FTIT_dataset* data , int id );
 
 #endif
--- src/util/ini.h
+++ src/util/ini.h
@@ -4,29 +4,188 @@
 #include "../../deps/iniparser/iniparser.h"
 #include "../../deps/iniparser/dictionary.h"
 
+/**--------------------------------------------------------------------------
+  
+  
+  @brief        File access mode for FTI_Iniparser function.
+
+  FTI_INI_OPEN: The filename passed to \ref FTI_Iniparser has to
+  be accessible, otherwise the initialization will fail.  
+
+
+  FTI_INI_CREATE: A new file is created. If file exists already,
+  it will be deleted.  
+  
+  
+  FTI_INI_APPEND: If file exists, only the changes will be applied.
+  If the file does not exists, a new file is created.
+
+
+--------------------------------------------------------------------------**/
 typedef enum FTIT_inimode {
-    FTI_INI_OPEN,
-    FTI_INI_CREATE,
-    FTI_INI_APPEND,
+
+    FTI_INI_OPEN,   /**< pass to FTI_Iniparser -> open existing file       */
+    FTI_INI_CREATE, /**< pass to FTI_Iniparser -> create new file          */
+    FTI_INI_APPEND, /**< pass to FTI_Iniparser -> append to existing file 
+    or create new one. */
+
 } FTIT_inimode;
 
+/**--------------------------------------------------------------------------
+  
+  
+  @brief        FTI_Iniparser handle.
+
+  This structure serves as the opaque iniparser handle. It contains the 
+  pointer to the dictionary and the belonging filepath.
+
+  The structure mimics C++ class behaviour to simplify the handling. The 
+  FTI functions that operate on the dictionary are members of this 
+  structure.
+
+
+--------------------------------------------------------------------------**/
 typedef struct FTIT_iniparser {
-    dictionary*                 dict;
-    char                        file[FTI_BUFS];
+
+    dictionary* dict;           /**< Pointer to iniparser dictionary       */
+    char        file[FTI_BUFS]; /**< Path to corresponding file            */
     char*       (*getString)    ( struct FTIT_iniparser*, const char* );
     int         (*getInt)       ( struct FTIT_iniparser*, const char* );
     int         (*getLong)      ( struct FTIT_iniparser*, const char* );
     int         (*set)          ( struct FTIT_iniparser*, const char*, const char* );
-    int         (*dump)        ( struct FTIT_iniparser* );
+    int         (*dump)         ( struct FTIT_iniparser* );
     int         (*clear)        ( struct FTIT_iniparser* );
+
 } FTIT_iniparser;
 
+/**--------------------------------------------------------------------------
+  
+  
+  @brief        Initializes instance of FTIT_iniparser.
+
+  This function initializes the dictionary. If mode = \ref FTI_INI_OPEN, 
+  the dictionary will be created by parsing the file passed to the function.
+  The same applies for mode = \ref FTI_INI_APPEND, if the file exists. If not,
+  the bahavior is the same as for mode = \ref FTI_INI_CREATE. 
+  If mode = FTI_INI_CREATE, the file passed to the function is created. If 
+  the file already exists, it will be removed.
+
+  @param        self[out]    <b> FTIT_iniparser* </b> Handle that needs to
+  be passed to the other FTI_Iniparser functions. Cannot be NULL.
+  @param        inifile[in]  <b> const char* </b> Path to ini file.
+  @param        mode[in]     <b> FTIT_inimode </b> File access mode.
+  
+  @return                       \ref FTI_SCES on success.  
+                                \ref FTI_NSCS upon failure.
+ 
+
+--------------------------------------------------------------------------**/
 int FTI_Iniparser( FTIT_iniparser*, const char*, FTIT_inimode );
+
+/**--------------------------------------------------------------------------
+  
+  
+  @brief        Requests string value for key.
+
+  This function returns the string value that is set for key. If key was not
+  found, the null string '\0' is returned.
+
+  @param        self[in]    <b> FTIT_iniparser* </b> FTI_Iniparser handle.
+  @param        key[in]     <b> const char* </b> dictionary key.
+  
+  @return                       Dictionary value on success.  
+                                Null string ('\0') if key not found.
+ 
+
+--------------------------------------------------------------------------**/
 char* FTI_IniparserGetString( FTIT_iniparser*, const char* );
+
+/**--------------------------------------------------------------------------
+  
+  
+  @brief        Requests integer value for key.
+
+  This function returns the integer value that is set for key. If key was not
+  found, -1 is returned.
+
+  @param        self[in]    <b> FTIT_iniparser* </b> FTI_Iniparser handle.
+  @param        key[in]     <b> const char* </b> dictionary key.
+  
+  @return                       Dictionary value on success.  
+                                <b> int </b> -1 if key not found.
+ 
+
+--------------------------------------------------------------------------**/
 int FTI_IniparserGetInt( FTIT_iniparser*, const char* key );
+
+/**--------------------------------------------------------------------------
+  
+  
+  @brief        Requests long integer value for key.
+
+  This function returns the long integer value that is set for key. If key was not
+  found, -1 is returned.
+
+  @param        self[in]    <b> FTIT_iniparser* </b> FTI_Iniparser handle.
+  @param        key[in]     <b> const char* </b> dictionary key.
+  
+  @return                       Dictionary value on success.  
+                                <b> long </b> -1 if key not found.
+ 
+
+--------------------------------------------------------------------------**/
 int FTI_IniparserGetLong( FTIT_iniparser*, const char* key );
+
+/**--------------------------------------------------------------------------
+  
+  
+  @brief        Sets key to val.
+
+  This function assigns val to key. If for val NULL is passed, the key
+  will be the name of a section in the iniparser file.
+
+  @param        self[in/out] <b> FTIT_iniparser* </b> FTI_Iniparser handle.
+  @param        key[in]      <b> const char* </b> dictionary key.
+  @param        val[in]      <b> const char* </b> key value.
+  
+  @return                       \ref FTI_SCES on success.  
+                                \ref FTI_NSCS on failure.
+
+ 
+--------------------------------------------------------------------------**/
 int FTI_IniparserSet( FTIT_iniparser*, const char*, const char* );
+
+/**--------------------------------------------------------------------------
+  
+  
+  @brief        Writes dictionary to file.
+
+  This function writes the dictionary to the iniparser file. 
+
+  @param        self[in] <b> FTIT_iniparser* </b> FTI_Iniparser handle.
+  
+  @return                       \ref FTI_SCES on success.  
+                                \ref FTI_NSCS upon failure.
+
+
+--------------------------------------------------------------------------**/
 int FTI_IniparserDump( FTIT_iniparser* );
+
+/**--------------------------------------------------------------------------
+  
+  
+  @brief        Destroys FTI_Iniparser instance.
+
+  This function frees all the memory allocated for the dictionary by calling
+  the corresponding iniparser library function.
+
+  @param        self[in] <b> FTIT_iniparser* </b> FTI_Iniparser handle.
+  
+  @return                       \ref FTI_SCES on success.  
+                                \ref FTI_NSCS upon failure.
+
+
+--------------------------------------------------------------------------**/
 int FTI_IniparserClear( FTIT_iniparser* );
 
 #endif // FTI_INI_H
