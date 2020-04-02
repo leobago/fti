ba14e5c61b4b07b8f983437a149815054dbf4247
alexandre de limas santana <alexandre.delimassantana@bsc.es>
2020-04-02 16:13:34 +0200

    Included all IOs in the Jenkins/Local tests

---
+++ testing/ci/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./install.sh --enable-sionlib --enable-testing --enable-hdf5 --disable-examples
--- testing/ci/localtests.sh
+++ testing/ci/localtests.sh
@@ -3,5 +3,7 @@
 # Run this scripts from the testing folder in build
 
 export MPIRUN_ARGS=--oversubscribe
-itf/testrunner $(find 'local' -name '*.fixture' | sed s/.fixture//)
+
+@itf_run_cmd@ $(find 'local' -name '*.fixture' | grep -v 'vpr' | sed s/.fixture//)
+
 unset MPIRUN_ARGS
