067f719d6cd1273b3cb4d4b85e9740509f66a62a
alexandre de limas santana <alexandre.delimassantana@bsc.es>
2020-04-02 18:35:56 +0200

    added sionlib integration into the jenkins build script

--- testing/ci/build.sh
+++ testing/ci/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./install.sh --enable-sionlib --enable-testing --enable-hdf5 --disable-examples
+./install.sh --enable-sionlib --enable-testing --enable-hdf5 --disable-examples --sionlib-path=/opt/sionlib
