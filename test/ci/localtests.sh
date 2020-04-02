15e68fb2068622dbba4a3a41086c7504411d4913
alexandre de limas santana <alexandre.delimassantana@bsc.es>
2020-04-02 15:30:42 +0200

    Added the h5_single_file_enable into the ITF config gile template and used it in the vpr fixture

--- test/ci/localtests.sh
+++ test/ci/localtests.sh
@@ -3,5 +3,6 @@
 # Run this scripts from the test folder in build
 
 export MPIRUN_ARGS=--oversubscribe
-itf/testrunner $(find 'local' -name '*.fixture' | sed s/.fixture//)
+itf/testrunner -v 2 local/variateProcessorRestart/vpr
+#itf/testrunner $(find 'local' -name '*.fixture' | sed s/.fixture//)
 unset MPIRUN_ARGS
