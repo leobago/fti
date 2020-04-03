2a6da6991faf030abe8d9f042d4a397f67a3af41
alexandre de limas santana <alexandre.delimassantana@bsc.es>
2020-04-03 14:21:44 +0200

    failproof test

--- testing/ci/localtests.sh
+++ testing/ci/localtests.sh
@@ -4,7 +4,8 @@
 
 export MPIRUN_ARGS=--oversubscribe
 
-fixtures=$(find '@testing_dir@/local' -name '*.fixture' | sed s/.fixture//)
+#fixtures=$(find '@testing_dir@/local' -name '*.fixture' | sed s/.fixture//)
+fixtures=$(find '@testing_dir@/local' -name '*.fixture' | grep 'failtime' | sed s/.fixture//)
 @itf_run_cmd@ ${fixtures[@]}
 retval=$?
 
