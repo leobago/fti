baaacd4216ead2a2fe9ee9083d4aff1ecc7fbeb4
alexandre de limas santana <alexandre.delimassantana@bsc.es>
2020-04-06 13:37:46 +0200

    Disabled the standard-disrupt temporairly

--- testing/ci/localtests.sh
+++ testing/ci/localtests.sh
@@ -4,7 +4,7 @@
 
 export MPIRUN_ARGS=--oversubscribe
 
-fixtures=$(find '@testing_dir@/local' -name '*.fixture' | sed s/.fixture//)
+fixtures=$(find '@testing_dir@/local' -name '*.fixture' | grep -v 'standard-disrupt' | sed s/.fixture//)
 @itf_run_cmd@ ${fixtures[@]}
 retval=$?
 
