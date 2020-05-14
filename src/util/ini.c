e1aad28ee17b463eac434fd9d297f2bb203195b6
Sawsane <sawsaneouchtal@gmail.com>
2020-05-14 19:40:32 +0200

    Code style fixed for src/util

--- src/util/ini.c
+++ src/util/ini.c
@@ -39,175 +39,145 @@
 
 #include "../interface.h"
 
-int FTI_Iniparser( FTIT_iniparser* self, const char* inifile, FTIT_inimode mode )
-{
-
+int FTI_Iniparser(FTIT_iniparser* self, const char* inifile,
+ FTIT_inimode mode) {
     char err[FTI_BUFS];
-    
-    if( self == NULL )
-    {
+
+    if (self == NULL) {
         FTI_Print("iniparser context is NULL.", FTI_EROR);
         return FTI_NSCS;
     }
 
-    if( inifile == NULL )
-    {
+    if (inifile == NULL) {
         FTI_Print("iniparser file is NULL.", FTI_EROR);
         return FTI_NSCS;
     }
 
     bool create = (mode == FTI_INI_CREATE) ? true : false;
-    
-    if ( access(inifile, R_OK) != 0 ) {
-        if( mode == FTI_INI_OPEN ) {
-            snprintf( err, FTI_BUFS, "Iniparser failed to open meta file ('%s').", inifile );
-            FTI_Print( err, FTI_DBUG );
+
+    if (access(inifile, R_OK) != 0) {
+        if (mode == FTI_INI_OPEN) {
+            snprintf(err, FTI_BUFS,
+             "Iniparser failed to open meta file ('%s').", inifile);
+            FTI_Print(err, FTI_DBUG);
             return FTI_NSCS;
-        } 
-        else if( mode == FTI_INI_APPEND ) {
+        }  else if (mode == FTI_INI_APPEND) {
             create = true;
         }
     }
 
     self->dict = NULL;
-    
-    if ( create ) { 
-        self->dict = dictionary_new(0); 
+
+    if (create) {
+        self->dict = dictionary_new(0);
+    } else {
+        self->dict = iniparser_load(inifile);
     }
-    else {
-        self->dict = iniparser_load( inifile );
-    }
-    
+
     if (self->dict == NULL) {
-        if( mode == FTI_INI_OPEN ) {
-            snprintf( err, FTI_BUFS, "Iniparser failed to parse the file ('%s').", inifile );
+        if (mode == FTI_INI_OPEN) {
+            snprintf(err, FTI_BUFS,
+             "Iniparser failed to parse the file ('%s').", inifile);
+        } else if (mode == FTI_INI_APPEND) {
+            snprintf(err, FTI_BUFS,
+             "Iniparser failed to parse the file ('%s').", inifile);
+        } else {
+            snprintf(err, FTI_BUFS, "Unknown iniparser mode.");
         }
-        else if( mode == FTI_INI_APPEND ) {
-            snprintf( err, FTI_BUFS, "Iniparser failed to parse the file ('%s').", inifile );
-        } 
-        else {
-            snprintf( err, FTI_BUFS, "Unknown iniparser mode." );
-        }
-        FTI_Print( err, FTI_WARN );
+        FTI_Print(err, FTI_WARN);
         return FTI_NSCS;
     }
-    
+
     self->getString = FTI_IniparserGetString;
     self->getInt = FTI_IniparserGetInt;
     self->getLong = FTI_IniparserGetLong;
     self->set = FTI_IniparserSet;
     self->dump = FTI_IniparserDump;
     self->clear = FTI_IniparserClear;
-    strncpy( self->file, inifile, FTI_BUFS );
+    strncpy(self->file, inifile, FTI_BUFS);
 
     return FTI_SCES;
-
 }
 
-int FTI_IniparserSet( FTIT_iniparser* self, const char* key, const char* val )
-{   
-    
-    if( self == NULL )
-    {
+int FTI_IniparserSet(FTIT_iniparser* self, const char* key, const char* val) {
+    if (self == NULL) {
         FTI_Print("iniparser context is NULL.", FTI_EROR);
         return FTI_NSCS;
     }
 
-    return (iniparser_set( self->dict, key, val ) == 0) ? FTI_SCES : FTI_NSCS;
-
+    return (iniparser_set(self->dict, key, val) == 0) ? FTI_SCES : FTI_NSCS;
 }
 
-char* FTI_IniparserGetString( FTIT_iniparser* self, const char* key )
-{   
-    
+char* FTI_IniparserGetString(FTIT_iniparser* self, const char* key) {
     static char nullstr = '\0';
-    
-    if( self == NULL )
-    {
+
+    if (self == NULL) {
         FTI_Print("iniparser context is NULL.", FTI_EROR);
         return &nullstr;
     }
- 
-    char* string = iniparser_getstring( self->dict, key, NULL );
 
-    if( string == NULL ) return &nullstr;
+    char* string = iniparser_getstring(self->dict, key, NULL);
+
+    if (string == NULL) return &nullstr;
 
     return string;
-    
 }
 
-int FTI_IniparserGetInt( FTIT_iniparser* self, const char* key )
-{   
-
-    if( self == NULL )
-    {
+int FTI_IniparserGetInt(FTIT_iniparser* self, const char* key) {
+    if (self == NULL) {
         FTI_Print("iniparser context is NULL.", FTI_EROR);
         return FTI_NSCS;
     }
 
-    return iniparser_getint( self->dict, key, -1 );
-    
+    return iniparser_getint(self->dict, key, -1);
 }
 
-int FTI_IniparserGetLong( FTIT_iniparser* self, const char* key )
-{   
-
-    if( self == NULL )
-    {
+int FTI_IniparserGetLong(FTIT_iniparser* self, const char* key) {
+    if (self == NULL) {
         FTI_Print("iniparser context is NULL.", FTI_EROR);
         return FTI_NSCS;
     }
 
-    return iniparser_getlint( self->dict, key, -1 );
-    
+    return iniparser_getlint(self->dict, key, -1);
 }
 
-int FTI_IniparserDump( FTIT_iniparser* self )
-{
-
-    if( self == NULL )
-    {
+int FTI_IniparserDump(FTIT_iniparser* self) {
+    if (self == NULL) {
         FTI_Print("iniparser context is NULL.", FTI_EROR);
         return FTI_NSCS;
     }
 
     char err[FTI_BUFS];
-    
+
     FILE* fd = fopen(self->file, "w");
     if (fd == NULL) {
-        
-        snprintf( err, FTI_BUFS, "Iniparser failed to open file '%s'", self->file);
-        FTI_Print( err, FTI_WARN );
+        snprintf(err, FTI_BUFS, "Iniparser failed to open file '%s'",
+         self->file);
+        FTI_Print(err, FTI_WARN);
 
         return FTI_NSCS;
-
     }
 
     // Write metadata
-    iniparser_dump_ini( self->dict , fd );
+    iniparser_dump_ini(self->dict , fd);
 
     if (fclose(fd) != 0) {
-
-        snprintf( err, FTI_BUFS, "Iniparser failed to close file '%s'", self->file);
-        FTI_Print( err, FTI_WARN );
+        snprintf(err, FTI_BUFS, "Iniparser failed to close file '%s'",
+         self->file);
+        FTI_Print(err, FTI_WARN);
 
         return FTI_NSCS;
     }
 
     return FTI_SCES;
-
 }
 
-int FTI_IniparserClear( FTIT_iniparser* self )
-{
-    
-    if( self == NULL )
-    {
+int FTI_IniparserClear(FTIT_iniparser* self) {
+    if (self == NULL) {
         FTI_Print("iniparser context is NULL.", FTI_EROR);
         return FTI_NSCS;
     }
 
-    iniparser_freedict( self->dict );
+    iniparser_freedict(self->dict);
     return FTI_SCES;
-
 }
