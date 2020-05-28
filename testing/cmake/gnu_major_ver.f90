584bb2207bd2234f386a05db1fbf8cba1452aaa9
Sawsane <sawsaneouchtal@gmail.com>
2020-05-28 18:04:54 +0200

    Code style fixed for /include and all Fortran files

--- testing/cmake/gnu_major_ver.f90
+++ testing/cmake/gnu_major_ver.f90
@@ -1,4 +1,4 @@
 program main
-    use iso_fortran_env, only : output_unit
-    write(output_unit,'(i2)') __GNUC__
+   use iso_fortran_env, only: output_unit
+   write (output_unit, '(i2)') __GNUC__
 end program
