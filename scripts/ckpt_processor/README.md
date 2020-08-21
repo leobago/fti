e932a69eb6a6d7424b8f0a2c6f43eba5e6f2b444
Sawsane <sawsaneouchtal@gmail.com>
2020-08-21 18:48:42 +0200

    fixed API input to config_file

--- scripts/ckpt_processor/README.md
+++ scripts/ckpt_processor/README.md
@@ -1,7 +1,7 @@
 # FTI Checkpoints Processor
 
 This program allows to read FTI's checkpoints from an external Python script. \
-Input: application root folder (application that is checkpointed using FTI) \
+Input: FTI's configuration file \
 Output: application data in CSV format
 
 This is an initial version of FTI's checkpoint processor
@@ -16,15 +16,14 @@ Pre-requisites: (to be relaxed in the next version)
 In FTI In configuration file, have the following parameters set as follows: \
 ckpt_io = 1; \
 ckpt_dir = ./local; \
-meta_dir = ./meta; \
+meta_dir = ./meta; 
 
 ## Usage
 
 ```python
 import read_fti_checkpoints
 
-read_fti_checkpoints.read_checkpoints(app_root) # returns 'words'
+read_fti_checkpoints.read_checkpoints(config_file) 
 
 ```
 
-Note: "app_root" is the directory where the application resides. It is important to have the meta/ and local/ folders directly under app_root for this version to work.
--- scripts/ckpt_processor/read_fti_ckpts.py
+++ scripts/ckpt_processor/read_fti_ckpts.py
@@ -9,15 +9,18 @@ import posix_read_ckpts
 
 pattern_ckpt = "*.fti"
 pattern_meta = "sector*"
-pattern_config = "*config*"
 ckpts = []
 metas = []
+ckpt_dir = ""
+meta_dir = ""
+ckpt_abs_path = ""
+meta_abs_path = ""
 config_file = ""
 execution_id = ""
 
 #takes list of meta files, processes the 
 #corresponding ckpt files per meta file
-def map_ckpt_to_meta(metas, app_root):
+def map_ckpt_to_meta(metas):
 	meta_to_ckpt_mapping = {}
 	for meta in metas:
 		configs = []
@@ -27,50 +30,95 @@ def map_ckpt_to_meta(metas, app_root):
 			if section.isdigit():
 				ckptfile = config[section]['ckpt_file_name']
 				print(ckptfile)
-				ckptfile = app_root+"/local/node"+str(section)+"/"+execution_id+"/l1/"+ckptfile
-				print("full path: ",ckptfile)
+				ckptfile = ckpt_abs_path+"/node"+str(section)+"/"+execution_id+"/l1/"+ckptfile
 				meta_to_ckpt_mapping[ckptfile] = meta
 	return meta_to_ckpt_mapping
 
 #reads the execution_id token for files extraction
-def get_execution_id(config_file):
-	print("reading config::", config_file)
+def init_config_params(config_file):
 	global execution_id
+	global ckpt_dir
+	global meta_dir
 	config = configparser.ConfigParser()
 	config.read(config_file)
 	#read group_size
 	execution_id = config['restart']['exec_id']
-	print("exec_id :", execution_id)
+	ckpt_dir = config['basic']['ckpt_dir']
+	meta_dir = config['basic']['meta_dir']
+
+def process_fti_paths(config_file):
+	global ckpt_dir
+	global meta_dir
+	global ckpt_abs_path
+	global meta_abs_path
+	#ckpt dir
+	dir_path = os.path.dirname(os.path.realpath(config_file))
+	print(dir_path)
+	#concatenate paths
+	if ckpt_dir.startswith('./') == True: #same directory as config
+		ckpt_abs_path = dir_path + ckpt_dir.replace('.','')
+	elif "." not in ckpt_dir: #absolute path
+		#directly change dirs to 
+		ckpt_abs_path = ckpt_dir
+	else: #relative path
+		#iterate over the number of '../' found in ckpt_path
+		os.chdir(dir_path)
+		dirs = ckpt_dir.count("..")
+		print("dirs : ", dirs)
+		for i in range(dirs):
+			os.chdir("..")
+		#concatenate the remaining part
+		for i in range(dirs):
+			#remove ../
+			ckpt_dir = ckpt_dir.replace('../','')
+		os.chdir(ckpt_dir)
+		ckpt_abs_path = os.getcwd()
+
+	#meta dir
+	dir_path = os.path.dirname(os.path.realpath(config_file))
+	print(dir_path)
+	#concatenate paths
+	if meta_dir.startswith('./') == True: #same directory as config
+		#omit dot + concatenate the rest of the path
+		meta_abs_path = dir_path + meta_dir.replace('.','')
+	elif "." not in meta_dir: #absolute path
+		#directly change dirs to 
+		meta_abs_path = meta_dir
+	else: #relative path
+		#iterate over the number of '../' found in ckpt_path
+		os.chdir(dir_path)
+		dirs = meta_dir.count("..")
+		print("dirs : ", dirs)
+		for i in range(dirs):
+			os.chdir("..")
+		#concatenate the remaining part
+		for i in range(dirs):
+			#remove ../
+			meta_dir = meta_dir.replace('../','')
+		os.chdir(meta_dir)
+		meta_abs_path = os.getcwd()
+
 
 #fetches fti's necessary files: config, metas, ckpts
-def read_fti_files(app_root):
-	if os.path.isdir(app_root) == True:
-		print("directory ", app_root, " exists")
+def read_fti_files(config_file):
+	process_fti_paths(config_file)
 	# get the ckpt files 
-	for path, subdirs, files in os.walk(app_root+"/local"):
+	for path, subdirs, files in os.walk(ckpt_abs_path):
 		for name in files:
 			if fnmatch(name, pattern_ckpt):
 				ckpts.append(os.path.join(path, name))
 				#TODO: check if ckpt file is empty before proceeding
 	# get the meta files
-	for path, subdirs, files in os.walk(app_root+"/meta"):
+	for path, subdirs, files in os.walk(meta_abs_path):
 		for name in files:
 			if fnmatch(name, pattern_meta):
 				metas.append(os.path.join(path, name))
 
-	#get the config file
-	for path, subdirs, files in os.walk(app_root):
-		for name in files:
-			if fnmatch(name, pattern_config):
-				metas.append(os.path.join(path, name))
-				config_file = os.path.join(path, name)
-	return config_file
-
 #reads the ckpts of given app
-def read_checkpoints(app_root):
-	config_file = read_fti_files(app_root) #populates metas+ckpts+config
-	get_execution_id(config_file) #sets execution_id
-	meta_to_ckpt_mapping = map_ckpt_to_meta(metas, app_root)
+def read_checkpoints(config_file):
+	init_config_params(config_file) #sets execution_id
+	read_fti_files(config_file) #populates metas+ckpts+config
+	meta_to_ckpt_mapping = map_ckpt_to_meta(metas)
 	for ckpt in meta_to_ckpt_mapping:
 		print("Processing ", ckpt, " using meta ", meta_to_ckpt_mapping[ckpt])
 		posix_read_ckpts.read_checkpoint(ckpt, meta_to_ckpt_mapping[ckpt], config_file)
