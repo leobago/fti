0ee44739dae1869f3362e3e509fcd592db0a4921
alexandre de limas santana <alexandre.delimassantana@bsc.es>
2020-09-09 19:50:51 +0200

    Added extra checks and protection on new FTI_GetType API function

--- src/util/tools.h
+++ src/util/tools.h
@@ -30,6 +30,7 @@ int FTI_Clean(FTIT_configuration* FTI_Conf, FTIT_topology* FTI_Topo,
         FTIT_checkpoint* FTI_Ckpt, int level);
 
 void FTI_CopyStringOrDefault(char* dest, char* src, char* fmt, ...);
+int FTI_IsTypeComplex(FTIT_type *t);
 FTIT_type* FTI_GetComplexType(fti_id_t handle);
 
 #ifdef __cplusplus
